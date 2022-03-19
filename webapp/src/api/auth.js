dc5b482d7af16eeb612e8d459a5000c57f022b94
Denilson V <deethedev@gmail.com>
2022-03-19 16:24:43 -0400

    Fix tab spacing

--- webapp/src/api/auth.js
+++ webapp/src/api/auth.js
@@ -3,11 +3,11 @@ import { API_PATH_START, API_PATH_VERIFY_PHONE } from "./apiPaths";
 
 // TODO: using temporary GET request, switch to POST when server api is done, uncomment data
 export const verifyPhone = (phone, otpCode) => get({
-    path: API_PATH_VERIFY_PHONE,
-    // data: { phone, token: otpCode }
+	path: API_PATH_VERIFY_PHONE,
+	// data: { phone, token: otpCode }
 });
 // TODO: using temporary GET request, switch to POST when server api is done
 export const start = (phone) => post({
-    path: API_PATH_START,
-    data: { phone }
+	path: API_PATH_START,
+	data: { phone }
 });
--- webapp/src/constants/errorMessages.js
+++ webapp/src/constants/errorMessages.js
@@ -1,7 +1,7 @@
 const errorMsg = {
-    invalidToken: "You entered the wrong code, try again.",
-    invalidTokenOrPhone: "Code is incorrect or phone number is not in our system. Try again.",
-    tooManyAttempts: "Too many attemps.",
+	invalidToken: "You entered the wrong code, try again.",
+	invalidTokenOrPhone: "Code is incorrect or phone number is not in our system. Try again.",
+	tooManyAttempts: "Too many attemps.",
 }
 
 export default errorMsg;
