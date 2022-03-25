aa9f5cc288bab971001e335c63b46ba49632fce6
Denilson V <deethedev@gmail.com>
2022-03-24 23:35:00 -0400

    Styling scooter icon and configure cluster

--- webapp/src/pages/Dashboard.js
+++ webapp/src/pages/Dashboard.js
@@ -51,11 +51,14 @@ function Map() {
       id: 'mapbox/streets-v11',
     }).addTo(map);
 
-    const createScooter = L.icon({
-      iconUrl: scooterIcon,
-      iconSize: [38, 38],
-      iconAnchor: [16, 38],
-      // popupAnchor: [-3, -76],
+    const createScooter = L.divIcon({
+      // TODO: load svg dynamically
+      html: `<svg id="ePJdIXVzjGA1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 121.21" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><g transform="translate(-193.037537-102.389076)"><rect width="100" height="100" rx="10" ry="10" transform="translate(193.037543 102.389078)" fill="#fafafa" stroke-width="0"/><rect width="40" height="40" rx="5" ry="5" transform="matrix(.707107 0.707107-.707107 0.707107 243.037543 169.104807)" fill="#fafafa" stroke-width="0"/></g></svg>
+        <img src="${scooterIcon}" class="scooterIcon"/>
+      `,
+      className: 'scooterContainer',
+      iconSize: [36, 36],
+      iconAnchor: [18, 36],
     });
     const getScooters = fetch("https://gbfs.spin.pm/api/gbfs/v2_2/portland/free_bike_status");
     getScooters
@@ -67,31 +70,36 @@ function Map() {
           removeOutsideVisibleBounds: true,
           disableClusteringAtZoom: 18,
           maxClusterRadius: 32,
-          iconCreateFunction: function (cluster) {
+          iconCreateFunction: (cluster) => {
             return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>', className: 'scooterCluster' });
           }
         });
         response.data.bikes.map((bike) => markers.addLayer(L.marker([bike.lat, bike.lon], { icon: createScooter })
           .on('click', (e) => {
             let { lat, lng } = e.latlng;
-            const currentZoom = map.getZoom();
-            // TODO: optimize or find efficient way to do this
-            // fixing: after defaultZoom, scooterImg appears lower consecutively
-            const defaultZoom = 20;
-            const zoomStable = 0.85;
-            // checks if map zoom is default or more/closer
-            const zoomTo = currentZoom > defaultZoom ? currentZoom : defaultZoom;
-            const largest = Math.max(currentZoom, defaultZoom);
-            const smallest = Math.min(currentZoom, defaultZoom);
-            // 1.85
-            const zoomDifference = currentZoom > defaultZoom ? (largest - smallest + zoomStable) : 0;
-            // 0000001.85
-            let lowerToValue = parseFloat(zoomDifference).toFixed(2).padStart(10, 0);
-            //0.000018500000000000002
-            lowerToValue /= Math.pow(10, 5);
-            // moves scooter view slightly lower to adjust to zoom changes after defaultZoom
-            lat = lat + 0.00005 - lowerToValue;
-            map.setView([lat, lng], zoomTo);
+            // TODO: remove following - Probably Unnecessary atm
+            // const currentZoom = map.getZoom();
+            // // TODO: optimize or find efficient way to do this
+            // // fixing: after defaultZoom, scooterImg appears lower consecutively
+            // const defaultZoom = 20;
+            // const zoomStable = 0.85;
+            // // checks if map zoom is default or more/closer
+            // const zoomTo = currentZoom > defaultZoom ? currentZoom : defaultZoom;
+            // const largest = Math.max(currentZoom, defaultZoom);
+            // const smallest = Math.min(currentZoom, defaultZoom);
+            // // 1.85
+            // const zoomDifference = currentZoom > defaultZoom ? (largest - smallest + zoomStable) : 0;
+            // // 0000001.85
+            // let lowerToValue = parseFloat(zoomDifference).toFixed(2).padStart(10, 0);
+            // //0.000018500000000000002
+            // lowerToValue /= Math.pow(10, 5);
+            // // moves scooter view slightly lower to adjust to zoom changes after defaultZoom
+            // lat = lat + 0.00005 - lowerToValue;
+            lat = lat + 0.000005;
+            map.flyTo([lat, lng], 23, {
+              animate: true,
+              duration: 1.5
+            });
           })));
         map.addLayer(markers, {
           chunkInterval: 350
