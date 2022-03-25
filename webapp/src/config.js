40c9ce819799a09fae00ca0cef5f9929e67af68f
Rob Galanakis <rob.galanakis@gmail.com>
2022-03-25 13:36:28 -0700

    Allow API host to be configured to root, set it during build

--- webapp/src/config.js
+++ webapp/src/config.js
@@ -1,5 +1,15 @@
+// If the API host is configured, use that.
+// If it's '/', assume we mean 'the same server',
+// and use an empty string. Otherwise, fall back to local dev,
+// which is usually a different server to the React dev server.
+let apiHost = process.env.REACT_APP_API_HOST;
+if (apiHost === "/") {
+  apiHost = "";
+} else if (!apiHost) {
+  apiHost = `http://localhost:22001`;
+}
 const config = {
-  apiHost: process.env.REACT_APP_API_HOST || `http://localhost:22001`,
+  apiHost: apiHost,
   chaos: process.env.REACT_APP_CHAOS,
   debug: process.env.REACT_APP_DEBUG,
   environment: process.env.NODE_ENV,
