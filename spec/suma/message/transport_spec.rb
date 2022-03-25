00e2c60f383f9c9b0cbd966e2a89a75fab0837c6
Rob Galanakis <rob.galanakis@gmail.com>
2022-03-25 13:20:20 -0700

    Transport.for should create a new transport instance

---
+++ spec/suma/message/transport_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "suma/message"
+require "suma/twilio"
+
+RSpec.describe Suma::Message::Transport, :db do
+  describe "for" do
+    it "returns a new transport instance" do
+      Suma::Message::SmsTransport.allowlist = ["99"]
+      t = described_class.for("sms")
+      expect(t).to be_a(Suma::Message::SmsTransport)
+      expect(t.allowlist).to eq(["99"])
+    ensure
+      Suma::Message::SmsTransport.reset_configuration
+    end
+
+    it "can error for an invalid type" do
+      expect(described_class.for("foo")).to be_nil
+      expect { described_class.for!("foo") }.to raise_error(Suma::Message::InvalidTransportError)
+    end
+
+    it "can use an override" do
+      described_class.override = "sms"
+      t = described_class.for("invalid")
+      expect(t).to be_a(Suma::Message::SmsTransport)
+    ensure
+      described_class.override = nil
+    end
+  end
+end
