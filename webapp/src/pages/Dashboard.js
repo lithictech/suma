99cc198f03f52b7a90441fed502ea374bef299e4
Denilson V <deethedev@gmail.com>
2022-03-29 13:03:22 -0400

    Styling changes and map config

--- webapp/src/pages/Dashboard.js
+++ webapp/src/pages/Dashboard.js
@@ -53,7 +53,7 @@ function Map() {
 
     const createScooter = L.divIcon({
       // TODO: load svg dynamically
-      html: `<svg id="ePJdIXVzjGA1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 121.21" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><g transform="translate(-193.037537-102.389076)"><rect width="100" height="100" rx="10" ry="10" transform="translate(193.037543 102.389078)" fill="#fafafa" stroke-width="0"/><rect width="40" height="40" rx="5" ry="5" transform="matrix(.707107 0.707107-.707107 0.707107 243.037543 169.104807)" fill="#fafafa" stroke-width="0"/></g></svg>
+      html: `<svg id="ePJdIXVzjGA1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 121.21" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><g transform="translate(-193.037537-102.389076)"><rect width="40" height="40" rx="5" ry="5" transform="matrix(.707107 0.707107-.707107 0.707107 243.037543 169.104807)" fill="#fafafa" stroke-width="0"/><rect width="100" height="100" rx="20" ry="20" transform="translate(193.037543 102.389078)" fill="#fafafa" stroke-width="0"/></g></svg>
         <img src="${scooterIcon}" class="scooterIcon"/>
       `,
       className: 'scooterContainer',
@@ -77,26 +77,9 @@ function Map() {
         response.data.bikes.map((bike) => markers.addLayer(L.marker([bike.lat, bike.lon], { icon: createScooter })
           .on('click', (e) => {
             let { lat, lng } = e.latlng;
-            // TODO: remove following - Probably Unnecessary atm
-            // const currentZoom = map.getZoom();
-            // // TODO: optimize or find efficient way to do this
-            // // fixing: after defaultZoom, scooterImg appears lower consecutively
-            // const defaultZoom = 20;
-            // const zoomStable = 0.85;
-            // // checks if map zoom is default or more/closer
-            // const zoomTo = currentZoom > defaultZoom ? currentZoom : defaultZoom;
-            // const largest = Math.max(currentZoom, defaultZoom);
-            // const smallest = Math.min(currentZoom, defaultZoom);
-            // // 1.85
-            // const zoomDifference = currentZoom > defaultZoom ? (largest - smallest + zoomStable) : 0;
-            // // 0000001.85
-            // let lowerToValue = parseFloat(zoomDifference).toFixed(2).padStart(10, 0);
-            // //0.000018500000000000002
-            // lowerToValue /= Math.pow(10, 5);
-            // // moves scooter view slightly lower to adjust to zoom changes after defaultZoom
-            // lat = lat + 0.00005 - lowerToValue;
-            lat = lat + 0.000005;
-            map.flyTo([lat, lng], 23, {
+            const lowerTo = 0.00004;
+            const loweredLat = lat + lowerTo;
+            map.flyTo([loweredLat, lng], 21, {
               animate: true,
               duration: 1.5
             });
@@ -109,6 +92,15 @@ function Map() {
   }, [])
   return (
     <div style={{ position: 'relative' }}>
+      <ReserveCard />
+      <div ref={mapRef} style={{ height: '500px' }} />
+    </div>
+  )
+}
+
+function ReserveCard() {
+  return (
+    <>
       {/* zIndex higher than map (400)*/}
       <Card style={{ position: 'absolute', top: 0, left: '50%', transform: 'translate(-50%, 2%)', zIndex: '401' }} className="mx-auto">
         <Card.Body>
@@ -117,7 +109,6 @@ function Map() {
           <Button size="sm" variant="outline-success" href="http://google.com" onClick={(e) => e.preventDefault()}>Reserve Scooter</Button>
         </Card.Body>
       </Card>
-      <div ref={mapRef} style={{ height: '500px' }} />
-    </div>
+    </>
   )
 }
