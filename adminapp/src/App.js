cba5e912e7b63a2fe7fcbb1c295268707111d003
Rob Galanakis <rob.galanakis@gmail.com>
2022-05-22 20:52:51 -0700

    Run lint and fmt

--- adminapp/src/App.js
+++ adminapp/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import React from "react";
 
 function App() {
   return (
--- adminapp/src/index.js
+++ adminapp/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from "./App";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
