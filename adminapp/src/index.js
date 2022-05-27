ceca65a51fa317c7b8d32a9a22fa93778c50a625
Rob Galanakis <rob.galanakis@gmail.com>
2022-05-27 13:05:58 -0700

    Add CssBaseline

--- adminapp/src/index.js
+++ adminapp/src/index.js
@@ -1,10 +1,12 @@
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
