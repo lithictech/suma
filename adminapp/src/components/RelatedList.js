7658366db1f9b3253eaa0c3bcc1b03103b6b3158
Denilson Velasquez <deethedev@gmail.com>
2022-06-02 12:12:21 -0400

    Re-refactor Members list and details

---
+++ adminapp/src/components/RelatedList.js
@@ -0,0 +1,19 @@
+import SimpleTable from "./SimpleTable";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import _ from "lodash";
+import React from "react";
+
+export default function RelatedList({ title, tableProps, ...rest }) {
+  tableProps = _.merge({ size: "small" }, tableProps);
+  return (
+    <Box mt={5}>
+      {title && (
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+      )}
+      <SimpleTable tableProps={tableProps} {...rest} />
+    </Box>
+  );
+}
