7d2847d468270279bb1c4def9d4cc3a0e94f65c9
Rob Galanakis <rob.galanakis@gmail.com>
2022-06-10 12:22:24 -0700

    Use 1-based pagination

--- webapp/src/components/ForwardBackPagination.js
+++ webapp/src/components/ForwardBackPagination.js
@@ -1,20 +1,24 @@
 import clsx from "clsx";
 import i18next from "i18next";
+import { clamp } from "lodash/number";
 import React from "react";
 import Pagination from "react-bootstrap/Pagination";
 
 export default function ForwardBackPagination({ page, pageCount, onPageChange }) {
+  const handlePageChange = (p) => {
+    onPageChange(clamp(p, 1, pageCount));
+  };
   return (
     <Pagination size="sm" className="justify-content-end">
       <Pagination.Prev
-        className={clsx(page <= 0 && "disabled")}
-        onClick={(_e) => onPageChange(page - 1)}
+        className={clsx(page <= 1 && "disabled")}
+        onClick={() => handlePageChange(page - 1)}
       >
         {i18next.t("pagination_prev", { ns: "common" })}
       </Pagination.Prev>
       <Pagination.Next
-        className={clsx(page + 1 >= pageCount && "disabled")}
-        onClick={(_e) => onPageChange(page + 1)}
+        className={clsx(page >= pageCount && "disabled")}
+        onClick={() => handlePageChange(page + 1)}
       >
         {i18next.t("pagination_next", { ns: "common" })}
       </Pagination.Next>
---
+++ webapp/src/modules/clamp.js
@@ -0,0 +1,9 @@
+export default function clamp(x, min, max) {
+  if (x < min) {
+    return min;
+  }
+  if (x > max) {
+    return max;
+  }
+  return x;
+}
