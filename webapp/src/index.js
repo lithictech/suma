19c88a19874f000118189222a7c71ef571ef0eb2
Rob Galanakis <rob.galanakis@gmail.com>
2022-07-03 21:22:50 -0700

    Install leaflet via npm, not script tags
    Fixes https://github.com/lithictech/suma/issues/148
    
    We aren't deferring it, but that's fine,
    we don't need to load it from a CDN as it's
    really just a normal package.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,6 @@
 import App from "./App";
 import "./assets/styles/imports.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "leaflet.animatedmarker/src/AnimatedMarker";
-import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/leaflet.markercluster";
 import React from "react";
 import ReactDOM from "react-dom";
 
