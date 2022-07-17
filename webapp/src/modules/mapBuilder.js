c1997121080beb4dc99a09be3a24b9dfb3b038ba
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2022-07-17 01:42:45 -0400

    Add geoFences and icon spidering to mobility map (#162)
    * Add geoFences to mobility map. Hard-coded for now. See #164 for backend features.
    * Spiderify markers with same location

--- webapp/src/modules/mapBuilder.js
+++ webapp/src/modules/mapBuilder.js
@@ -1,6 +1,7 @@
 import api from "../api";
 import scooterIcon from "../assets/images/kick-scooter.png";
 import scooterContainer from "../assets/images/scooter-container.svg";
+import { t } from "../localization";
 import leaflet from "leaflet";
 import "leaflet.animatedmarker/src/AnimatedMarker";
 import "leaflet.markercluster/dist/MarkerCluster.css";
@@ -12,7 +13,8 @@ export default class MapBuilder {
     this.mapRef = mapRef;
     this._l = leaflet;
     this._minZoom = 13;
-    this._maxZoom = 20;
+    this._maxZoom = 23;
+    this._zoomTo = 20;
     this._dLat = 45.5152;
     this._dLng = -122.6784;
     this._latOffset = 0.00004;
@@ -33,6 +35,7 @@ export default class MapBuilder {
     this.setTileLayer();
     this.setScooterIcon();
     this.setMarkerCluster();
+    this.loadGeoFences();
     return this;
   }
 
@@ -65,11 +68,11 @@ export default class MapBuilder {
 
   setMarkerCluster() {
     this._mcg = this._l.markerClusterGroup({
-      spiderfyOnMaxZoom: false,
       showCoverageOnHover: false,
-      removeOutsideVisibleBounds: true,
-      disableClusteringAtZoom: 18,
-      maxClusterRadius: 32,
+      maxClusterRadius: (mapZoom) => {
+        // only cluster same location markers above zoom 17
+        return mapZoom >= 17 ? 0 : 32;
+      },
       iconCreateFunction: (cluster) => {
         return this._l.divIcon({
           html: "<b>" + cluster.getChildCount() + "</b>",
@@ -80,8 +83,7 @@ export default class MapBuilder {
   }
 
   loadScooters({ onVehicleClick }) {
-    this.setEventHandlers(onVehicleClick);
-    this.getScooters(onVehicleClick);
+    this.setEventHandlers(onVehicleClick).getScooters(onVehicleClick);
     this._map.addLayer(this._mcg);
     return this;
   }
@@ -131,9 +133,158 @@ export default class MapBuilder {
           });
         });
         this._mcg.addLayers(newMarkers, { chunkedLoading: true });
-        this.stopRefreshTimer();
-        this.startRefreshTimer(r.data.refresh, onVehicleClick);
+        this.stopRefreshTimer().startRefreshTimer(r.data.refresh, onVehicleClick);
       });
+  }
+
+  loadGeoFences() {
+    const apiResponse = {
+      doNotParkOrRide: [
+        [
+          [45.49584550855579, -122.68355369567871],
+          [45.49576278304519, -122.68331229686737],
+          [45.496168888931074, -122.68292605876921],
+          [45.49607488319949, -122.68264710903166],
+          [45.49703749446685, -122.68198192119598],
+          [45.49724806348807, -122.68259346485138],
+          [45.49600719897556, -122.68352150917053],
+          [45.495977117072165, -122.6834625005722],
+          [45.49584550855579, -122.68355369567871],
+        ],
+      ],
+      doNotRide: [
+        [
+          [45.50015270758223, -122.68442273139952],
+          [45.49948719071191, -122.68540441989899],
+          [45.49901906820077, -122.68492430448532],
+          [45.500212866911646, -122.68348127603531],
+          [45.50048734303642, -122.68365025520323],
+          [45.500530582303945, -122.68397212028502],
+          [45.50015270758223, -122.68442273139952],
+        ],
+      ],
+      doNotPark: [
+        [
+          [45.497545, -122.685026],
+          [45.4971314, -122.68488],
+          [45.497045, -122.6847],
+          [45.496717, -122.684948],
+          [45.49598, -122.68461],
+          [45.495777, -122.684347],
+          [45.495785, -122.684004],
+          [45.4959169, -122.683655],
+          [45.495728, -122.683194],
+          [45.4960335, -122.682896],
+          [45.4959827, -122.68276],
+          [45.4956349, -122.682848],
+          [45.4956349, -122.682496],
+          [45.496185, -122.68174],
+          [45.4965937, -122.68137],
+          [45.497022, -122.681212],
+          [45.4971051, -122.681268],
+          [45.497308, -122.681343],
+          [45.497797, -122.682384],
+          [45.49768, -122.68306],
+          [45.4979512, -122.683435],
+          [45.4978, -122.683945],
+          [45.497481, -122.684344],
+          [45.49751, -122.6846],
+          [45.4976071, -122.68474],
+          [45.497545, -122.685026],
+        ],
+        [
+          [45.50206179495491, -122.68491994589567],
+          [45.50153071609439, -122.68461316823958],
+          [45.501344602317154, -122.68466949462892],
+          [45.50120172667681, -122.68466010689735],
+          [45.500931013942854, -122.68455684185028],
+          [45.500531522287645, -122.68468961119653],
+          [45.50026456628402, -122.68468961119653],
+          [45.50019312713877, -122.68461316823958],
+          [45.500535282222344, -122.68412500619888],
+          [45.50083513620411, -122.68403112888336],
+          [45.50136340171654, -122.68418535590172],
+          [45.501996937805096, -122.68425107002257],
+          [45.50212665203002, -122.68437042832375],
+          [45.50216613021309, -122.6845595240593],
+          [45.50206179495491, -122.68491994589567],
+        ],
+      ],
+    };
+    Promise.resolve(apiResponse).then((r) => {
+      if (r.doNotPark) {
+        this.createRestrictedArea({
+          latlngs: r.doNotPark,
+          options: { restriction: "parking" },
+        });
+      }
+      if (r.doNotRide) {
+        this.createRestrictedArea({
+          latlngs: r.doNotRide,
+          options: { restriction: "riding" },
+        });
+      }
+      if (r.doNotParkOrRide) {
+        this.createRestrictedArea({
+          latlngs: r.doNotParkOrRide,
+          options: { restriction: "all" },
+        });
+      }
+    });
+  }
+
+  createRestrictedArea({ latlngs, options }) {
+    options = options || {};
+    let popup = this._l.popup({
+      direction: "top",
+      offset: [0, -5],
+    });
+    let polygonFillOpacity = 0.3;
+    const parkingRestrictionContent = `<h6 class='mb-0'>${t(
+      "mobility:do_not_park_title"
+    )}</h6><p class='m-0'>${t("mobility:do_not_park_intro")}</p>`;
+    const ridingRestrictionContent = `<h6 class='mb-0'>${t(
+      "mobility:do_not_ride_title"
+    )}</h6><p class='m-0'>${t("mobility:do_not_ride_intro")}</p>`;
+    const allRestrictionsContent =
+      parkingRestrictionContent + "<hr />" + ridingRestrictionContent;
+    if (options.restriction === "parking") {
+      popup.setContent(parkingRestrictionContent);
+      polygonFillOpacity = 0.2;
+    }
+    if (options.restriction === "riding") {
+      popup.setContent(ridingRestrictionContent);
+      polygonFillOpacity = 0.2;
+    }
+    if (options.restriction === "all") {
+      popup.setContent(allRestrictionsContent);
+    }
+    const restrictedIcon = this._l.divIcon({
+      iconAnchor: [12, 12],
+      iconSize: [24, 24],
+      className: "mobility-restricted-area-icon",
+      html: "<i class='bi bi-slash-circle'></i>",
+    });
+
+    latlngs.forEach((area) => {
+      const restrictedMarker = this._l
+        .marker(this._l.latLngBounds(area).getCenter(), {
+          icon: restrictedIcon,
+          interactive: false,
+        })
+        .bindPopup(popup)
+        .addTo(this._map);
+      this._l
+        .polygon([area], {
+          fillOpacity: polygonFillOpacity,
+          color: "#b53d00",
+          weight: 1,
+        })
+        .on("click", () => {
+          restrictedMarker.openPopup();
+        })
+        .addTo(this._map);
+    });
   }
 
   startRefreshTimer(interval, onVehicleClick) {
@@ -149,6 +300,7 @@ export default class MapBuilder {
       clearInterval(this._refreshId);
       this._refreshId = null;
     }
+    return this;
   }
 
   // Remove markers in visible bounds to prevent duplicates
@@ -202,12 +354,13 @@ export default class MapBuilder {
   }
 
   beginTrip({ onGetLocation, onGetLocationError }) {
+    this._mcg.clearLayers();
     this.tripMode();
     let loc, line;
     this._map
       .locate({
         watch: true,
-        maxZoom: this._maxZoom,
+        maxZoom: this._zoomTo,
         timeout: 20000,
         enableHighAccuracy: true,
       })
@@ -219,8 +372,7 @@ export default class MapBuilder {
       .on("locationfound", (e) => {
         if (!loc) {
           loc = e.latlng;
-          this._mcg.clearLayers();
-          this._map.setView([e.latitude + this._latOffset, e.longitude], this._maxZoom);
+          this._map.setView([e.latitude + this._latOffset, e.longitude], this._zoomTo);
           line = this._l.polyline([[e.latlng.lat, e.latlng.lng]]);
           this._tripMarker = this._l.animatedMarker(line.getLatLngs(), {
             icon: this._scooterIcon,
@@ -245,7 +397,7 @@ export default class MapBuilder {
           this._tripMarker.options.distance = nextDistance;
           line.addLatLng([e.latitude, e.longitude]);
           this._tripMarker.start();
-          this._map.flyTo([e.latitude + this._latOffset, e.longitude], this._maxZoom, {
+          this._map.flyTo([e.latitude + this._latOffset, e.longitude], this._zoomTo, {
             animate: true,
             duration: 0.25,
           });
