56d301fb86b99997d24dae0f18e398cb9cc7fa43
Denilson V <deethedev@gmail.com>
2022-09-01 19:04:49 -0400

    refactor updateMarker and scooter icon style

--- webapp/src/modules/mapBuilder.js
+++ webapp/src/modules/mapBuilder.js
@@ -39,234 +39,33 @@ export default class MapBuilder {
     this._scooterIcon = this._l.divIcon({
       html: `<img src="${scooterContainer}" alt="scooter container"/><img src="${scooterIcon}" class="mobility-map-icon-img" alt="scooter icon"/>`,
       className: "mobility-map-icon",
-      iconSize: [43.4, 52.6],
-      iconAnchor: [21.7, 52.6],
+      iconSize: [42, 50],
+      iconAnchor: [21, 50],
     });
-    this._vehicleClicked = false;
+    this._lastLocation = null;
     this._locationMarker = null;
     this._locationAccuracyCircle = null;
     this._animationTimeoutId = null;
     this._refreshId = null;
+    this._vehicleClicked = null;
     this._onVehicleClick = null;
+    this._onVehicleRemove = null;
   }
 
   init() {
-    this.setTileLayer();
-    this.setLocationEventHandlers();
-    this.loadGeoFences();
+    this._setTileLayer();
+    this._setLocationEventHandlers();
+    this._loadGeoFences();
     return this;
   }
 
-  setTileLayer() {
-    this._l
-      .tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-        {
-          maxZoom: this._maxZoom,
-          minZoom: this._minZoom,
-          tileSize: 512,
-          zoomOffset: -1,
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          id: "mapbox/streets-v11",
-        }
-      )
-      .addTo(this._map);
-  }
-
-  setLocationEventHandlers() {
-    this._map.on("zoomstart", () => {
-      if (this._locationAccuracyCircle && this._locationMarker) {
-        // prevent animation issues when zooming
-        if (this._animationTimeoutId) {
-          clearTimeout(this._animationTimeoutId);
-          this._animationTimeoutId = null;
-        }
-        this._locationAccuracyCircle._path.classList.remove(
-          "mobility-location-accuracy-circle-animation"
-        );
-        this._locationMarker._icon.style.transition = "none";
-      }
-    });
-    this._map.on("zoomend", () => {
-      if (this._locationAccuracyCircle && this._locationMarker) {
-        this._animationTimeoutId = setTimeout(() => {
-          this._locationAccuracyCircle._path.classList.add(
-            "mobility-location-accuracy-circle-animation"
-          );
-          this._locationMarker._icon.style.transition = "all 1000ms linear 0s";
-        }, 250);
-      }
-    });
-  }
-
-  setVehicleEventHandlers() {
-    this._map.on("moveend", this.moveEnd, this);
-    this._map.on("click", this.click, this);
-  }
-
-  moveEnd() {
-    if (!this._map) {
-      return;
-    }
-    const bounds = this._map.getBounds();
-    if (
-      !this._lastBounds.contains(bounds._northEast) ||
-      !this._lastBounds.contains(bounds._southWest)
-    ) {
-      this._lastBounds = bounds;
-      this.getScooters(bounds);
-    }
-  }
-
-  click() {
-    if (this._vehicleClicked) {
-      this._onVehicleClick(null);
-      this._vehicleClicked = false;
-    }
-  }
-
-  loadScooters({ onVehicleClick }) {
+  loadScooters({ onVehicleClick, onVehicleRemove }) {
     this._onVehicleClick = onVehicleClick;
-    this.getScooters(this._lastBounds);
-    this.setVehicleEventHandlers();
+    this._onVehicleRemove = onVehicleRemove;
+    this._getAndUpdateScooters(this._lastBounds, this._mcg);
+    this._setVehicleEventHandlers();
     this._map.addLayer(this._mcg);
     return this;
-  }
-
-  getScooters(bounds) {
-    api.getMobilityMap(boundsToParams(bounds)).then((r) => {
-      const precisionFactor = 1 / r.data.precision;
-      const newMarkers = [];
-      this.removeVisibleLayers(bounds);
-      ["ebike", "escooter"].forEach((vehicleType) => {
-        r.data[vehicleType]?.forEach((bike) => {
-          const marker = this.newMarker(
-            bike,
-            vehicleType,
-            r.data.providers,
-            precisionFactor
-          );
-          newMarkers.push(marker);
-        });
-      });
-      this._mcg.addLayers(newMarkers, { chunkedLoading: true });
-      this.stopRefreshTimer().startRefreshTimer(r.data.refresh);
-    });
-  }
-
-  loadGeoFences() {
-    // TODO: Need to use new bounds logic
-    const bounds = this._l.latLngBounds(
-      this._l.latLng(45.40706339656264, -122.80156150460245),
-      this._l.latLng(45.58041884450583, -122.51986645843971)
-    );
-    return api.getMobilityMapFeatures(boundsToParams(bounds)).then((d) => {
-      d.data.restrictions.forEach((r) => {
-        this.createRestrictedArea({
-          latlngs: r.polygon,
-          restriction: r.restriction,
-        });
-      });
-    });
-  }
-
-  createRestrictedArea({ latlngs, restriction }) {
-    let popup = this._l.popup({
-      direction: "top",
-      offset: [0, -5],
-    });
-    let polygonFillOpacity = 0.3;
-    const parkingRestrictionContent = `<h6 class='mb-0'>${t(
-      "mobility:do_not_park_title"
-    )}</h6><p class='m-0'>${t("mobility:do_not_park_intro")}</p>`;
-    const ridingRestrictionContent = `<h6 class='mb-0'>${t(
-      "mobility:do_not_ride_title"
-    )}</h6><p class='m-0'>${t("mobility:do_not_ride_intro")}</p>`;
-    const allRestrictionsContent =
-      parkingRestrictionContent + "<hr />" + ridingRestrictionContent;
-    if (restriction === "do-not-park") {
-      popup.setContent(parkingRestrictionContent);
-      polygonFillOpacity = 0.2;
-    }
-    if (restriction === "do-not-ride") {
-      popup.setContent(ridingRestrictionContent);
-      polygonFillOpacity = 0.2;
-    }
-    if (restriction === "do-not-park-or-ride") {
-      popup.setContent(allRestrictionsContent);
-    }
-    const restrictedIcon = this._l.divIcon({
-      iconAnchor: [12, 12],
-      iconSize: [24, 24],
-      className: "mobility-restricted-area-icon",
-      html: "<i class='bi bi-slash-circle'></i>",
-    });
-
-    const restrictedMarker = this._l
-      .marker(this._l.latLngBounds(latlngs).getCenter(), {
-        icon: restrictedIcon,
-      })
-      .bindPopup(popup)
-      .addTo(this._map);
-    this._l
-      .polygon([latlngs], {
-        fillOpacity: polygonFillOpacity,
-        color: "#b53d00",
-        weight: 1,
-      })
-      .on("click", () => {
-        restrictedMarker.openPopup();
-      })
-      .addTo(this._map);
-  }
-
-  startRefreshTimer(interval) {
-    if (!this._refreshId && !this._ongoingTrip) {
-      this._refreshId = window.setInterval(() => {
-        this.getScooters(this._lastBounds);
-      }, interval);
-    }
-  }
-
-  stopRefreshTimer() {
-    if (this._refreshId) {
-      clearInterval(this._refreshId);
-      this._refreshId = null;
-    }
-    return this;
-  }
-
-  // Remove markers in visible bounds to prevent duplicates
-  removeVisibleLayers(bounds) {
-    const removableMarkers = [];
-    this._mcg.eachLayer((marker) => {
-      if (bounds.contains(marker._latlng)) {
-        removableMarkers.push(marker);
-      }
-    });
-    this._mcg.removeLayers(removableMarkers);
-  }
-
-  newMarker(bike, vehicleType, providers, precisionFactor) {
-    const [lat, lng] = bike.c;
-    return this._l
-      .marker([lat * precisionFactor, lng * precisionFactor], {
-        icon: this._scooterIcon,
-        riseOnHover: true,
-      })
-      .on("click", (e) => {
-        this.centerLocation(e.latlng);
-        const mapVehicle = {
-          loc: bike.c,
-          type: vehicleType,
-          disambiguator: bike.d,
-          providerId: providers[bike.p].id,
-        };
-        this._onVehicleClick(mapVehicle);
-        this._vehicleClicked = true;
-      });
   }
 
   startTrackingLocation({ onGetLocation, onGetLocationError }) {
@@ -320,7 +119,7 @@ export default class MapBuilder {
           });
           this._map.addLayer(this._locationAccuracyCircle);
           this._map.addLayer(this._locationMarker);
-          this.centerLocation({ ...loc, targetZoom: 15 });
+          this._centerLocation({ ...loc, targetZoom: 15 });
           onGetLocation(location);
         }
         if (
@@ -348,17 +147,304 @@ export default class MapBuilder {
   }
 
   beginTrip() {
-    // will be re-enabled on getScooters
-    this._map.off("moveend", this.moveEnd, this);
-    this._map.off("click", this.click, this);
+    // will be re-enabled on getAndUpdateScooters
+    this._map.off("moveend", this._moveEndEvent, this);
+    this._map.off("click", this._clickEvent, this);
     this._mcg.clearLayers();
-    this.stopRefreshTimer();
+    this._stopRefreshTimer();
     if (this._locationMarker) {
-      this.centerLocation(this._locationMarker.getLatLng());
+      this._centerLocation(this._locationMarker.getLatLng());
     }
   }
 
-  centerLocation({ lat, lng, targetZoom }) {
+  unmount() {
+    this._stopRefreshTimer();
+    this._map.stopLocate();
+  }
+
+  _setTileLayer() {
+    this._l
+      .tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+        {
+          maxZoom: this._maxZoom,
+          minZoom: this._minZoom,
+          tileSize: 512,
+          zoomOffset: -1,
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          id: "mapbox/streets-v11",
+        }
+      )
+      .addTo(this._map);
+  }
+
+  _setLocationEventHandlers() {
+    if (!this._locationAccuracyCircle && !this._locationMarker) {
+      return;
+    }
+    this._map.on("zoomstart", () => {
+      // prevent animation issues when zooming
+      if (this._animationTimeoutId) {
+        clearTimeout(this._animationTimeoutId);
+        this._animationTimeoutId = null;
+      }
+      this._locationAccuracyCircle._path.classList.remove(
+        "mobility-location-accuracy-circle-animation"
+      );
+      this._locationMarker.getElement().style.transition = "none";
+    });
+    this._map.on("zoomend", () => {
+      this._animationTimeoutId = setTimeout(() => {
+        this._locationAccuracyCircle._path.classList.add(
+          "mobility-location-accuracy-circle-animation"
+        );
+        this._locationMarker._icon.style.transition = "all 1000ms linear 0s";
+      }, 250);
+    });
+  }
+
+  _setVehicleEventHandlers() {
+    this._map.on("moveend", this._moveEndEvent, this);
+    this._map.on("click", this._clickEvent, this);
+  }
+
+  _moveEndEvent() {
+    const bounds = this._map.getBounds();
+    if (!this._map || this._lastBounds.contains(bounds)) {
+      return;
+    }
+    this._lastBounds = bounds;
+    this._getAndUpdateScooters(this._lastBounds, this._mcg);
+  }
+
+  _clickEvent() {
+    if (!this._vehicleClicked) {
+      return;
+    }
+    this._onVehicleClick(null);
+    this._vehicleClicked = null;
+  }
+
+  _getAndUpdateScooters(bounds, mcg) {
+    bounds = expandBounds(bounds, 0.05);
+    api.getMobilityMap(boundsToParams(bounds)).then((r) => {
+      this._updateMarkers({ ...r, bounds, mcg });
+      this._stopRefreshTimer()._startRefreshTimer(r.data.refresh);
+    });
+  }
+
+  _updateMarkers({ data, bounds, mcg }) {
+    const precisionFactor = 1 / data.precision;
+    const applicableMarkers = [];
+    const allNewMarkersIds = [];
+    const leftoverMarkers = [];
+    // First: Removes markers that are not present in the bounds
+    const removableMarkers = mcg
+      .getLayers()
+      .filter((marker) => !bounds.contains(marker._latlng));
+    mcg.removeLayers(removableMarkers, { chunkedLoading: true });
+    // Second: Add markers for ids that are missing
+    const currentMarkersIds = mcg.getLayers().map((marker) => marker.options.id);
+    ["ebike", "escooter"].forEach((vehicleType) => {
+      data[vehicleType]?.forEach((bike) => {
+        const id = `${bike.p}-${bike.c[0] * precisionFactor}-${
+          bike.c[1] * precisionFactor
+        }`;
+        const marker = this._newMarker(
+          id,
+          bike,
+          vehicleType,
+          data.providers,
+          precisionFactor
+        );
+        if (!currentMarkersIds.includes(id)) {
+          applicableMarkers.push(marker);
+        } else {
+          leftoverMarkers.push(marker);
+        }
+        allNewMarkersIds.push(id);
+      });
+    });
+    mcg.addLayers(applicableMarkers, { chunkedLoading: true });
+    // Third: Remove *leftover* markers that are not present in the new list of ids
+    // Leftover markers are visible in new bounds but might not exist in new list of ids,
+    // therefor we should remove the non-existing leftover marker(s)
+    const removableLeftoverMarkers = leftoverMarkers.filter(
+      (marker) => !allNewMarkersIds.includes(marker.options.id)
+    );
+    mcg.removeLayers(removableLeftoverMarkers, { chunkedLoading: true });
+
+    // Fourth: Close the map reserve card if the marker for a scooter is now gone
+    const removedMarkers = removableMarkers.concat(removableLeftoverMarkers);
+    const isVehicleRemoved = removedMarkers.find(
+      (marker) => this._vehicleClicked?.options.id === marker.options.id
+    );
+    if (!this._vehicleClicked || !isVehicleRemoved) {
+      return;
+    }
+    this._onVehicleRemove();
+    this._vehicleClicked = null;
+  }
+
+  _newMarker(id, bike, vehicleType, providers, precisionFactor) {
+    const [lat, lng] = bike.c;
+    return this._l
+      .marker([lat * precisionFactor, lng * precisionFactor], {
+        icon: this._scooterIcon,
+        riseOnHover: true,
+        id,
+      })
+      .on("click", (e) => {
+        this._centerLocation(e.latlng);
+        const mapVehicle = {
+          loc: bike.c,
+          type: vehicleType,
+          disambiguator: bike.d,
+          providerId: providers[bike.p].id,
+        };
+        this._onVehicleClick(mapVehicle);
+        this._vehicleClicked = e.target;
+      });
+  }
+
+  _loadGeoFences() {
+    // TODO: Need to use new bounds logic
+    const bounds = this._l.latLngBounds(
+      this._l.latLng(45.40706339656264, -122.80156150460245),
+      this._l.latLng(45.58041884450583, -122.51986645843971)
+    );
+    return api.getMobilityMapFeatures(boundsToParams(bounds)).then((d) => {
+      d.data.restrictions.forEach((r) => {
+        this._createRestrictedArea({
+          latlngs: r.polygon,
+          restriction: r.restriction,
+        });
+      });
+    });
+  }
+
+  _createRestrictedArea({ latlngs, restriction }) {
+    let popup = this._l.popup({
+      direction: "top",
+      offset: [0, -5],
+    });
+    let polygonFillOpacity = 0.3;
+    const parkingRestrictionContent = `<h6 class='mb-0'>${t(
+      "mobility:do_not_park_title"
+    )}</h6><p class='m-0'>${t("mobility:do_not_park_intro")}</p>`;
+    const ridingRestrictionContent = `<h6 class='mb-0'>${t(
+      "mobility:do_not_ride_title"
+    )}</h6><p class='m-0'>${t("mobility:do_not_ride_intro")}</p>`;
+    const allRestrictionsContent =
+      parkingRestrictionContent + "<hr />" + ridingRestrictionContent;
+    if (restriction === "do-not-park") {
+      popup.setContent(parkingRestrictionContent);
+      polygonFillOpacity = 0.2;
+    }
+    if (restriction === "do-not-ride") {
+      popup.setContent(ridingRestrictionContent);
+      polygonFillOpacity = 0.2;
+    }
+    if (restriction === "do-not-park-or-ride") {
+      popup.setContent(allRestrictionsContent);
+    }
+    const restrictedIcon = this._l.divIcon({
+      iconSize: [24, 24],
+      iconAnchor: [12, 12],
+      className: "mobility-restricted-area-icon",
+      html: "<i class='bi bi-slash-circle'></i>",
+    });
+
+    const restrictedMarker = this._l
+      .marker(this._l.latLngBounds(latlngs).getCenter(), {
+        icon: restrictedIcon,
+      })
+      .bindPopup(popup)
+      .addTo(this._map);
+    this._l
+      .polygon([latlngs], {
+        fillOpacity: polygonFillOpacity,
+        color: "#b53d00",
+        weight: 1,
+      })
+      .on("click", () => {
+        restrictedMarker.openPopup();
+      })
+      .addTo(this._map);
+  }
+
+  _startRefreshTimer(interval) {
+    if (this._refreshId && this._ongoingTrip) {
+      return;
+    }
+    this._refreshId = window.setInterval(() => {
+      this._getAndUpdateScooters(this._lastBounds, this._mcg);
+    }, interval);
+  }
+
+  _stopRefreshTimer() {
+    if (!this._refreshId) {
+      return this;
+    }
+    clearInterval(this._refreshId);
+    this._refreshId = null;
+    return this;
+  }
+
+  _setLocateControl() {
+    const LocateControl = this._l.Control.extend({
+      options: {
+        position: "topleft",
+        link: undefined,
+        center: () => {
+          this._centerLocation({ ...this._lastLocation, targetZoom: 15 });
+        },
+      },
+      onAdd() {
+        const container = leaflet.DomUtil.create(
+          "div",
+          "leaflet-control-locate leaflet-bar leaflet-control"
+        );
+        const link = leaflet.DomUtil.create(
+          "a",
+          "leaflet-bar-part leaflet-bar-part-single",
+          container
+        );
+        this.options.link = link;
+        link.href = "#";
+        link.title = "Locate me";
+        link.setAttribute("role", "button");
+        leaflet.DomUtil.create("div", "bi bi-geo-fill", link);
+        leaflet.DomEvent.on(
+          this.options.link,
+          "click",
+          () => this.options.center(),
+          this
+        );
+        leaflet.DomEvent.on(this.options.link, "dblclick", (ev) => {
+          leaflet.DomEvent.stopPropagation(ev);
+        });
+        return container;
+      },
+      onRemove() {
+        leaflet.DomEvent.off(
+          this.options.link,
+          "click",
+          () => this.options.center(),
+          this
+        );
+        leaflet.DomEvent.off(this.options.link, "dblclick", (ev) => {
+          leaflet.DomEvent.stopPropagation(ev);
+        });
+      },
+    });
+    this._l.control.locate = () => new LocateControl();
+    this._l.control.locate().addTo(this._map);
+  }
+
+  _centerLocation({ lat, lng, targetZoom }) {
     lat = Number(lat);
     lng = Number(lng);
     targetZoom = _.isUndefined(targetZoom) ? 18 : targetZoom;
@@ -375,11 +461,6 @@ export default class MapBuilder {
       });
     }
   }
-
-  unmount() {
-    this.stopRefreshTimer();
-    this._map.stopLocate();
-  }
 }
 
 function boundsToParams(bounds) {
@@ -388,4 +469,18 @@ function boundsToParams(bounds) {
     sw: [_southWest.lat, _southWest.lng],
     ne: [_northEast.lat, _northEast.lng],
   };
+}
+
+function expandBounds(bounds, distance) {
+  if (!bounds) {
+    return {};
+  }
+  if (!distance) {
+    return bounds;
+  }
+  bounds._northEast.lat += distance;
+  bounds._northEast.lng += distance;
+  bounds._southWest.lat -= distance;
+  bounds._southWest.lng -= distance;
+  return bounds;
 }
