87f07bd9a9162db5388c486f7ad0c8ab204bb3e1
Rob Galanakis <rob.galanakis@gmail.com>
2022-11-19 11:22:46 -0800

    Automation for auto-onboarding
    Fixes https://github.com/lithictech/suma/issues/257
    
    Also update the onboarding screen for when users
    are automatically onboarded, we won't show them the
    "wait 1-2 days" screen.

--- webapp/src/pages/OnboardingFinish.js
+++ webapp/src/pages/OnboardingFinish.js
@@ -1,11 +1,15 @@
 import { mdp, t } from "../localization";
+import { useUser } from "../state/useUser";
 import React from "react";
 import Button from "react-bootstrap/Button";
 
 export default function OnboardingFinish() {
+  const { user } = useUser();
   return (
     <>
-      {mdp("onboarding:finish_md")}
+      {user.onboarded
+        ? mdp("onboarding:finish_onboarded_md")
+        : mdp("onboarding:finish_md")}
       <div className="button-stack">
         <Button href="/dashboard" variant="outline-primary" className="mt-3">
           {t("common:okay_ex")}
