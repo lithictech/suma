b9c5af9dddc5d0f8d5c7a3fd23d2827ee112d2e9
Denilson Velasquez <deethedev@gmail.com>
2022-12-06 13:58:55 -0500

    Fix privacy policy topnav overlap bug

--- webapp/src/components/PrivacyPolicyContent.js
+++ webapp/src/components/PrivacyPolicyContent.js
@@ -16,6 +16,7 @@ import "../assets/styles/privacy-policy.scss";
 import ELink from "../components/ELink";
 import ScreenLoader from "../components/ScreenLoader";
 import { Lookup, t as loct } from "../localization";
+import { useGlobalViewState } from "../state/useGlobalViewState";
 import TranslationToggle from "./TranslationToggle";
 import ScrollSpy from "bootstrap/js/src/scrollspy";
 import clsx from "clsx";
@@ -32,6 +33,8 @@ import { Helmet } from "react-helmet-async";
 export default function PrivacyPolicyContent({ mobile }) {
   mobile = Boolean(mobile);
   const [i18nextLoading, setI18NextLoading] = React.useState(true);
+  const { topNav } = useGlobalViewState();
+  const topNavHeight = topNav?.clientHeight || 0;
   React.useEffect(() => {
     // initialize isolated privacy policy translations
     Promise.delayOr(500, i18n.loadNamespaces("privacy-policy-strings")).then(() => {
@@ -70,7 +73,10 @@ export default function PrivacyPolicyContent({ mobile }) {
             <TableOfContentsNav id="desktop-scrollspy" />
           </Col>
         )}
-        <div className={clsx("sticky-top", !mobile && "container d-xl-none")}>
+        <div
+          className={clsx("sticky-top", !mobile && "container d-xl-none")}
+          style={{ top: topNavHeight, zIndex: 1 }}
+        >
           <TableOfContentsNav id="mobile-scrollspy" mobile={true} />
         </div>
         <Container
