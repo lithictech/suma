60726078a00c0479348556ce27a6e82c90aa5b68
Denilson Velasquez <deethedev@gmail.com>
2023-01-20 16:59:59 -0500

    Run formatter

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,7 +4,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
