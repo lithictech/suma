4b7be3abb110b12e940d36817b4b69db93696ad4
Denilson Velasquez <deethedev@gmail.com>
2023-01-26 20:06:02 -0500

    Refactor onbeforeinstallprompt event

--- webapp/src/components/AddToHomescreenPopup.js
+++ webapp/src/components/AddToHomescreenPopup.js
@@ -2,10 +2,8 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 
 export default function AddToHomescreenPopup() {
-  const [beforeInstallPromptEvent, setBeforeInstallPromptEvent] = React.useState(
-    getBeforeInstallPromptEvent()
-  );
   const [isAppInstalled, setIsAppInstalled] = React.useState(false);
+  const addToHomescreenButton = React.useRef(null);
 
   // function isStandalone() {
   //   // check IOS usage
@@ -18,47 +16,36 @@ export default function AddToHomescreenPopup() {
   //   return true;
   // }
 
-  function handleAddToHomescreenPrompt() {
-    if (!beforeInstallPromptEvent) {
-      return;
-    }
-    return beforeInstallPromptEvent
-      .prompt()
-      .then(() => beforeInstallPromptEvent.userChoice)
-      .then((choiceResult) => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("user accepted the A2HS prompt");
-          setIsAppInstalled(true);
-        } else {
-          console.log("user dismissed the A2HS prompt");
-        }
-        setBeforeInstallPromptEvent(null);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }
+  const initNavigatorEventHandlers = React.useCallback(() => {
+    // TODO: Check navigator session . id to return; if prompt was already used (installed app)
+    // this can be done by storing in localstorage
 
-  function getBeforeInstallPromptEvent() {
     if ("onbeforeinstallprompt" in window) {
       window.addEventListener("beforeinstallprompt", (event) => {
         // Prevent early prompt display
         event.preventDefault();
-        return event;
+        addToHomescreenButton.current.addEventListener("click", () => {
+          event
+            .prompt()
+            .then(() => event.userChoice)
+            .then((choiceResult) => {
+              if (choiceResult.outcome === "accepted") {
+                console.log("user accepted the A2HS prompt");
+                setIsAppInstalled(true);
+              } else {
+                console.log("user dismissed the A2HS prompt");
+              }
+            })
+            .catch((err) => {
+              console.log(err.message);
+            });
+        })
       });
     }
-    console.log("Event not found or set.");
-    return null;
-  }
-
-  const initNavigatorEventHandlers = React.useCallback(() => {
-    // TODO: Check navigator session . id to return; if prompt was already used (installed app)
-    // this can be done by storing in localstorage
     if ("onappinstalled" in window) {
       window.addEventListener("appinstalled", () => {
         console.log("A2HS installed");
         setIsAppInstalled(true);
-        // TODO: Add condition to return if app is already installed
       });
     }
   }, []);
@@ -72,8 +59,7 @@ export default function AddToHomescreenPopup() {
       ) : (
         <p>App has not been installed on this device.</p>
       )}
-      {beforeInstallPromptEvent}
-      <Button onClick={() => handleAddToHomescreenPrompt()}>Add to homescreen</Button>
+      <Button ref={addToHomescreenButton}>Add to homescreen</Button>
     </>
   );
 }
