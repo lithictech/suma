7d8904f7da7c670aa351db62d67e6e54e5f674fe
Denilson Velasquez <deethedev@gmail.com>
2023-01-30 12:49:48 -0500

    Add loading button and conditions

--- webapp/src/components/AddToHomescreen.js
+++ webapp/src/components/AddToHomescreen.js
@@ -3,12 +3,15 @@ import config from "../config";
 import { t } from "../localization";
 import { localStorageCache } from "../shared/localStorageHelper";
 import useLocalStorageState from "../shared/react/useLocalStorageState";
+import PageLoader from "./PageLoader";
 import React from "react";
 import { Alert } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 export default function AddToHomescreen() {
   const [canPrompt, setCanPrompt] = useLocalStorageState(ADD_TO_HOMESCREEN_KEY, true);
+  const [hasRegistration, setHasRegistration] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
   const addToHomescreenButtonRef = React.useRef(null);
 
   const updateLocalStorage = React.useCallback(
@@ -19,72 +22,93 @@ export default function AddToHomescreen() {
     [setCanPrompt]
   );
 
+  const installPrompt = React.useCallback(
+    (event) => {
+      setIsLoading(true);
+      return event
+        .prompt()
+        .then(() => event.userChoice)
+        .then((result) => {
+          if (result.outcome === "accepted") {
+            updateLocalStorage(false);
+          }
+        })
+        .catch((err) => {
+          if (err.message.indexOf("The app is already installed") > -1) {
+            updateLocalStorage(false);
+          }
+          return err;
+        })
+        .finally(() => setIsLoading(false));
+    },
+    [updateLocalStorage]
+  );
+
   const initEventHandlers = React.useCallback(() => {
-    if (!addToHomescreenButtonRef.current) {
-      return;
-    }
-    if (canPrompt && isCompatible()) {
-      window.addEventListener("beforeinstallprompt", (event) => {
-        // Prevent early prompt display
-        event.preventDefault();
-        // TODO: fix multiple eventListeners being added per render
-        addToHomescreenButtonRef.current.addEventListener("click", () => {
-          updateLocalStorage(false);
-          return event
-            .prompt()
-            .then(() => event.userChoice)
-            .then((result) => {
-              if (result.outcome !== "accepted") {
-                updateLocalStorage(true);
-              }
-            })
-            .catch((err) => {
-              if (err.message.indexOf("The app is already installed") > -1) {
-                updateLocalStorage(false);
-              }
-              return err;
-            });
-        });
-      });
-    }
-    if ("onappinstalled" in window) {
-      window.addEventListener("appinstalled", () => updateLocalStorage(false));
-    }
     setTimeout(() => {
       navigator.serviceWorker
         .getRegistration(config.apiHost)
         .then((sw) => {
-          if (!sw) {
-            updateLocalStorage(false);
+          if (sw) {
+            setHasRegistration(true);
           }
         })
-        .catch((_e) => updateLocalStorage(false));
+        .catch((_e) => setHasRegistration(false));
     }, 100);
-  }, [canPrompt, updateLocalStorage]);
+
+    if (
+      !hasRegistration ||
+      !addToHomescreenButtonRef.current ||
+      (!canPrompt && !isCompatible())
+    ) {
+      return;
+    }
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent early prompt display
+      event.preventDefault();
+      addToHomescreenButtonRef.current.addEventListener("click", () =>
+        installPrompt(event)
+      );
+    });
+    if ("onappinstalled" in window) {
+      window.addEventListener("appinstalled", () => updateLocalStorage(false));
+    }
+  }, [canPrompt, hasRegistration, updateLocalStorage, installPrompt]);
   React.useEffect(initEventHandlers, [initEventHandlers]);
 
-  if (!canPrompt || !isCompatible()) {
+  if ((!canPrompt || !isCompatible()) && !hasRegistration) {
     return null;
   }
   return (
-    <Alert
-      variant="primary"
-      show={canPrompt}
-      onClose={() => updateLocalStorage(false)}
-      dismissible
-    >
-      <Alert.Heading>
-        <img src={sumaLogo} alt="MySuma Logo" className="me-2" style={{ width: 50 }} />
-        {t("common:add_to_homecreen")}
-      </Alert.Heading>
-      <p>{t("common:add_to_homescreen_intro")}</p>
-      <div className="d-flex justify-content-end">
-        <Button ref={addToHomescreenButtonRef} variant="primary">
-          <i className="bi bi-box-arrow-down me-1"></i>
-          {t("common:install_suma")}
-        </Button>
-      </div>
-    </Alert>
+    <>
+      {isLoading ? (
+        <PageLoader relative="true" />
+      ) : (
+        <Alert
+          variant="primary"
+          show={canPrompt}
+          onClose={() => updateLocalStorage(false)}
+          dismissible
+        >
+          <Alert.Heading>
+            <img
+              src={sumaLogo}
+              alt="MySuma Logo"
+              className="me-2"
+              style={{ width: 50 }}
+            />
+            {t("common:add_to_homecreen")}
+          </Alert.Heading>
+          <p>{t("common:add_to_homescreen_intro")}</p>
+          <div className="d-flex justify-content-end">
+            <Button ref={addToHomescreenButtonRef} variant="primary">
+              <i className="bi bi-box-arrow-down me-1"></i>
+              {t("common:install_suma")}
+            </Button>
+          </div>
+        </Alert>
+      )}
+    </>
   );
 }
 
