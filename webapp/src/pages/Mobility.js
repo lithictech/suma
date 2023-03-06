8b150831da348cee267d0b5748e1e2b5972dca45
Denilson Velasquez <deethedev@gmail.com>
2023-03-06 15:29:57 -0500

    Make mobility feature configurable

--- webapp/src/pages/Mobility.js
+++ webapp/src/pages/Mobility.js
@@ -2,30 +2,43 @@ import mobilityHeaderImage from "../assets/images/onboarding-mobility.jpg";
 import ExternalLink from "../components/ExternalLink";
 import WaitingListPage from "../components/WaitingListPage";
 import Map from "../components/mobilitymap/Map";
+import config from "../config";
 import { t } from "../localization";
 import externalLinks from "../modules/externalLinks";
+import { LayoutContainer } from "../state/withLayout";
 import React from "react";
 
 export default function Mobility() {
-  return (
+  return config.featureMobility ? (
     <>
-      <div className="pb-4">
-        <WaitingListPage
-          feature="mobility"
-          imgSrc={mobilityHeaderImage}
-          imgAlt="Scooter Mobility"
-          title={t("mobility:title")}
-          text={
-            <>
-              <span className="pe-2">{t("mobility:intro")}</span>
-              <ExternalLink href={externalLinks.mobilityInfoLink}>
-                {t("common:learn_more")}
-              </ExternalLink>
-            </>
-          }
-        />
-      </div>
+      <LayoutContainer top gutters>
+        <h5>{t("mobility:title")}</h5>
+        <p className="text-secondary">
+          {t("mobility:intro")}
+          <br />
+          <ExternalLink href={externalLinks.mobilityInfoLink}>
+            {t("common:learn_more")}
+          </ExternalLink>
+        </p>
+      </LayoutContainer>
       <Map />
     </>
+  ) : (
+    <div className="pb-4">
+      <WaitingListPage
+        feature="mobility"
+        imgSrc={mobilityHeaderImage}
+        imgAlt="Scooter Mobility"
+        title={t("mobility:title")}
+        text={
+          <>
+            <span className="pe-2">{t("mobility:intro")}</span>
+            <ExternalLink href={externalLinks.mobilityInfoLink}>
+              {t("common:learn_more")}
+            </ExternalLink>
+          </>
+        }
+      />
+    </div>
   );
 }
