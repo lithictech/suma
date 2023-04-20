dba048e6a7bdc631d640d08788154ac6070b6be9
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2023-04-19 22:55:35 -0400

    Remove unnecessary import

--- webapp/src/pages/Start.js
+++ webapp/src/pages/Start.js
@@ -5,7 +5,7 @@ import FormError from "../components/FormError";
 import { t } from "../localization";
 import useI18Next from "../localization/useI18Next";
 import { dayjs } from "../modules/dayConfig";
-import { formatPhoneNumber, numberToUs } from "../modules/numberFormatter";
+import { formatPhoneNumber } from "../modules/numberFormatter";
 import { Logger } from "../shared/logger";
 import useToggle from "../shared/react/useToggle";
 import { extractErrorCode, useError } from "../state/useError";
