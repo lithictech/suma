6e05c78dac951530acb281f3e54b4c4c1cb7eade
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2023-04-19 22:53:11 -0400

    Refactor numberFormatter and tests

--- webapp/src/modules/numberFormatter.js
+++ webapp/src/modules/numberFormatter.js
@@ -1,37 +1,23 @@
-export const formatPhoneNumber = (num, previousNum) => {
-  if (!num) {
-    return "";
-  }
-  if (num.startsWith("0") || num.startsWith("1")) {
+export const formatPhoneNumber = (num) => {
+  if (!num || num.startsWith("0") || num.startsWith("1")) {
     return "";
   }
   let currentNum = num.replace(/\D/g, "");
-  // in case we paste a num with US region code "1", we should remove it before formatting
-  if (currentNum.startsWith("1")) {
+  // in case `currentNum` starts with US region code "1", remove it before formatting
+  // also prevent starting with 0
+  if (currentNum.startsWith("1") || currentNum.startsWith("0")) {
     currentNum = currentNum.slice(1);
   }
-  const cvLength = currentNum.length;
-  if (!previousNum || num.length > previousNum.length) {
-    if (cvLength < 4) {
-      return currentNum;
-    } else if (cvLength < 7) {
-      return `(${currentNum.slice(0, 3)}) ${currentNum.slice(3)}`;
-    } else {
-      return `(${currentNum.slice(0, 3)}) ${currentNum.slice(3, 6)}-${currentNum.slice(
-        6,
-        10
-      )}`;
-    }
-  }
-  // while deleting, if num length equals 3 we remove parenthesis
-  if (cvLength === 3) {
+  if (currentNum.length < 4) {
     return currentNum;
-  }
-  // while deleting, if num length equals 6 we remove dash
-  if (cvLength === 6) {
+  } else if (currentNum.length < 7) {
     return `(${currentNum.slice(0, 3)}) ${currentNum.slice(3)}`;
+  } else {
+    return `(${currentNum.slice(0, 3)}) ${currentNum.slice(3, 6)}-${currentNum.slice(
+      6,
+      10
+    )}`;
   }
-  return num;
 };
 
 export const numberToUs = (num) => {
--- webapp/src/modules/numberFormatter.test.js
+++ webapp/src/modules/numberFormatter.test.js
@@ -1,39 +1,27 @@
 import { formatPhoneNumber, numberToUs } from "./numberFormatter";
 
-describe("typing number", () => {
-  it("only formats numbers", () => {
-    expect(formatPhoneNumber("e", "")).toEqual("");
-    expect(formatPhoneNumber("+", "")).toEqual("");
-    expect(formatPhoneNumber(" ", "")).toEqual("");
-  });
-  it("returns empty string when numbers start with 1 and 0", () => {
-    expect(formatPhoneNumber("1", "")).toEqual("");
-    expect(formatPhoneNumber("0", "")).toEqual("");
-  });
-  it("formats phone number", () => {
-    expect(formatPhoneNumber("222", "22")).toEqual("222");
-    expect(formatPhoneNumber("(222) 3", "(222)")).toEqual("(222) 3");
-    expect(formatPhoneNumber("(222) 3334", "(222) 333")).toEqual("(222) 333-4");
-    expect(formatPhoneNumber("(222) 333-4444", "(222) 333-444")).toEqual(
-      "(222) 333-4444"
-    );
-  });
-  it("converts formatted number to US region", () => {
-    expect(numberToUs(formatPhoneNumber("(222) 333-4444"))).toEqual("+12223334444");
-  });
-  it("can format US region number", () => {
-    expect(formatPhoneNumber("+12223334444")).toEqual("(222) 333-4444");
-  });
-  it("formats number without previousNumber parameter", () => {
-    expect(formatPhoneNumber("2223334444")).toEqual("(222) 333-4444");
-  });
+it("returns empty string when value starts with 1, 0, or not a number", () => {
+  expect(formatPhoneNumber("e")).toEqual("");
+  expect(formatPhoneNumber("+")).toEqual("");
+  expect(formatPhoneNumber(" ")).toEqual("");
+  expect(formatPhoneNumber("1")).toEqual("");
+  expect(formatPhoneNumber("0")).toEqual("");
 });
-
-describe("deleting number", () => {
-  it("formats phone number", () => {
-    expect(formatPhoneNumber("(222) 333-444", "(222) 333-4444")).toEqual("(222) 333-444");
-    expect(formatPhoneNumber("(222) 333-", "(222) 333-4")).toEqual("(222) 333");
-    expect(formatPhoneNumber("(222) ", "(222) 3")).toEqual("222");
-    expect(formatPhoneNumber("22", "222")).toEqual("22");
-  });
+it("formats phone number", () => {
+  expect(formatPhoneNumber("222")).toEqual("222");
+  expect(formatPhoneNumber("(222) 3")).toEqual("(222) 3");
+  expect(formatPhoneNumber("(222) 3334")).toEqual("(222) 333-4");
+  expect(formatPhoneNumber("(222) 333-4444")).toEqual("(222) 333-4444");
+});
+it("remove first character when formatted phone number that start with 1 or 0", () => {
+  expect(formatPhoneNumber("(022)")).toEqual("22");
+  expect(formatPhoneNumber("(122) 2")).toEqual("222");
+  expect(formatPhoneNumber("(022) 222-2")).toEqual("(222) 222");
+  expect(formatPhoneNumber("(122) 222-22")).toEqual("(222) 222-2");
+});
+it("converts formatted number to US region", () => {
+  expect(numberToUs(formatPhoneNumber("(222) 333-4444"))).toEqual("+12223334444");
+});
+it("can format US region number", () => {
+  expect(formatPhoneNumber("+12223334444")).toEqual("(222) 333-4444");
 });
