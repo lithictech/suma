ae827c7927ecfb984c318fc91dbcbeb933861879
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2023-05-09 17:47:03 -0400

    Update contact list external links

--- webapp/src/components/ContactListTags.js
+++ webapp/src/components/ContactListTags.js
@@ -2,13 +2,12 @@ import { t } from "../localization";
 import ExternalLink from "./ExternalLink";
 import React from "react";
 import Stack from "react-bootstrap/Stack";
-import { Link } from "react-router-dom";
 
 export default function ContactListTags() {
   return (
     <Stack direction="vertical" className="mt-4 text-center">
-      <Link to="/privacy-policy">{t("common:privacy_policy")}</Link>
-      <ExternalLink href="">Instagram</ExternalLink>
+      <ExternalLink href="/privacy-policy">{t("common:privacy_policy")}</ExternalLink>
+      <ExternalLink href="https://www.instagram.com/mysuma/">Instagram</ExternalLink>
     </Stack>
   );
 }
