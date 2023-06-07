84dc10c6e2c89261c83360dc53d3f9c1e5470e13
Rob Galanakis <rob.galanakis@gmail.com>
2023-06-07 11:55:30 -0700

    usecurrentLanguage hook sets cached value

--- webapp/src/localization/currentLanguage.js
+++ webapp/src/localization/currentLanguage.js
@@ -1,8 +1,13 @@
 import { localStorageCache } from "../shared/localStorageHelper";
 import useLocalStorageState from "../shared/react/useLocalStorageState";
+import React from "react";
 
 let cachedLanguage = localStorageCache.getItem("language", "en");
 
+/**
+ * Return the current language. Use only when outside of React;
+ * inside of React use the useCurrentLanguage hook.
+ */
 export function getCurrentLanguage() {
   return cachedLanguage;
 }
@@ -12,6 +17,13 @@ export function setCurrentLanguage(value) {
 }
 
 export function useCurrentLanguage() {
-  const [language, setLanguage] = useLocalStorageState("language", "en");
+  const [language, setLanguageInner] = useLocalStorageState("language", cachedLanguage);
+  const setLanguage = React.useCallback(
+    (value) => {
+      cachedLanguage = value;
+      setLanguageInner(value);
+    },
+    [setLanguageInner]
+  );
   return [language, setLanguage];
 }
