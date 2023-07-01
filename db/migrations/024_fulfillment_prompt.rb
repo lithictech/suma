680f630093408e08bb977aae2dd2a82a735874ce
Rob Galanakis <rob.galanakis@gmail.com>
2023-06-30 19:32:17 -0700

    Fix broken tests by avoiding insert on migration

--- db/migrations/024_fulfillment_prompt.rb
+++ db/migrations/024_fulfillment_prompt.rb
@@ -6,18 +6,20 @@ Sequel.migration do
       add_foreign_key :fulfillment_prompt_id, :translated_texts, null: true
       add_foreign_key :fulfillment_confirmation_id, :translated_texts, null: true
     end
-    prompt_id = from(:translated_texts).insert(
-      en: "How do you want to get your stuff?",
-      es: "¿Cómo desea obtener sus cosas?",
-    )
-    confirm_id = from(:translated_texts).insert(
-      en: "How you’re getting it",
-      es: "Cómo lo está recibiendo",
-    )
-    from(:commerce_offerings).update(
-      fulfillment_prompt_id: prompt_id,
-      fulfillment_confirmation_id: confirm_id,
-    )
+    if ENV["RACK_ENV"] != "test"
+      prompt_id = from(:translated_texts).insert(
+        en: "How do you want to get your stuff?",
+        es: "¿Cómo desea obtener sus cosas?",
+      )
+      confirm_id = from(:translated_texts).insert(
+        en: "How you’re getting it",
+        es: "Cómo lo está recibiendo",
+      )
+      from(:commerce_offerings).update(
+        fulfillment_prompt_id: prompt_id,
+        fulfillment_confirmation_id: confirm_id,
+      )
+    end
     alter_table(:commerce_offerings) do
       set_column_not_null :fulfillment_prompt_id
       set_column_not_null :fulfillment_confirmation_id
