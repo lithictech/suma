68072eab8779fa90fe941b2ce58001d9237c9dcd
Rob Galanakis <rob.galanakis@gmail.com>
2023-07-04 17:01:39 -0700

    Unambiguous constraint supports booleans

--- lib/sequel/unambiguous_constraint.rb
+++ lib/sequel/unambiguous_constraint.rb
@@ -3,16 +3,20 @@
 require "sequel"
 
 module Sequel
-  def self.unambiguous_constraint(columns, allow_all_null: false)
+  def self.unambiguous_constraint(columns, allow_all_null: false, compare_to: nil)
     raise ArgumentError, "must provide at least one column" if columns.empty?
     lines = columns.map do |outer_col|
       conds = columns.map do |inner_col|
-        outer_col == inner_col ? Sequel[inner_col] !~ nil : Sequel[inner_col] =~ nil
+        outer_col == inner_col ? Sequel[inner_col] !~ compare_to : Sequel[inner_col] =~ compare_to
       end
       conds[1..].inject(conds.first) { |memo, expr| memo & expr }
     end
     lines << columns[1..].inject(Sequel[columns.first] =~ nil) { |memo, c| memo & (Sequel[c] =~ nil) } if
       allow_all_null
     return lines[1..].inject(lines.first) { |memo, expr| memo | expr }
+  end
+
+  def self.unambiguous_bool_constraint(columns, allow_all_null: false)
+    return self.unambiguous_constraint(columns, allow_all_null:, compare_to: false)
   end
 end
