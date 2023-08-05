f177091e89ff94aa8bf9e944334a4cd5990006b3
Rob Galanakis <rob.galanakis@gmail.com>
2023-08-04 23:42:09 -0400

    Fix busted migration due to existing rows on staging

--- db/migrations/031_supported_currencies_maximum_cents.rb
+++ db/migrations/031_supported_currencies_maximum_cents.rb
@@ -3,7 +3,11 @@
 Sequel.migration do
   change do
     alter_table(:supported_currencies) do
-      add_column :funding_maximum_cents, :integer, null: false
+      add_column :funding_maximum_cents, :integer, null: true
+    end
+    from(:supported_currencies).update(funding_maximum_cents: 100_00)
+    alter_table(:supported_currencies) do
+      set_column_not_null :funding_maximum_cents
     end
   end
 end
