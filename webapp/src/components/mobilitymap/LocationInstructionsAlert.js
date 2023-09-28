8b5d7d84b3cb77a79113502e49316878612df39e
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2023-09-28 18:58:34 -0400

    Refactor the way links are displayed
    * Update supported browser and platforms and matching externalLink
    * generate links if either browser and/or platform links exist
    * Refactor translations
    * Manual test in different browsers for accuracy

--- webapp/src/components/mobilitymap/LocationInstructionsAlert.js
+++ webapp/src/components/mobilitymap/LocationInstructionsAlert.js
@@ -2,6 +2,8 @@ import api from "../../api";
 import { md, t } from "../../localization";
 import { useCurrentLanguage } from "../../localization/currentLanguage";
 import externalLinks from "../../modules/externalLinks";
+import useMountEffect from "../../shared/react/useMountEffect";
+import useToggle from "../../shared/react/useToggle";
 import ExternalLink from "../ExternalLink";
 import SumaButton from "../SumaButton";
 import clsx from "clsx";
@@ -10,51 +12,42 @@ import React from "react";
 import Alert from "react-bootstrap/Alert";
 
 const LocationInstructionsAlert = () => {
-  const [userAgent, setUserAgent] = React.useState({});
-  const [linkKey, setLinkKey] = React.useState("");
-  const [icon, setIcon] = React.useState("");
+  const [buttonProperties, setButtonProperties] = React.useState([]);
+  const isLoading = useToggle(true);
   const [language] = useCurrentLanguage();
-  React.useEffect(() => {
-    if (!isEmpty(userAgent)) {
-      return;
-    }
+  useMountEffect(() => {
     api
       .getUserAgent()
       .then(api.pickData)
       .then((ua) => {
-        // get the second word in case for "Microsoft Edge"
-        const browser = ua.device.toLowerCase().split(" ")[1] || ua.device.toLowerCase();
-        if (ua.isIos) {
-          setLinkKey("ios");
-          setIcon("bi-apple");
-        } else if (ua.isAndroid) {
-          setLinkKey(`android`);
-          setIcon("bi-android");
-        } else if (supportedBrowsers.includes(browser)) {
-          setLinkKey(browser);
-          setIcon(`bi-browser-${browser}`);
-        }
-        setUserAgent(ua);
+        setButtonProperties(supportedBrowserAndPlatform(ua));
+        isLoading.turnOff();
       });
-  }, [userAgent]);
-  const locationInstructionsMissing = !linkKey && !isEmpty(userAgent);
+  });
   return (
     <Alert variant="warning" className="m-0 fs-6">
       <i className="bi bi-exclamation-triangle-fill"></i> {t("errors:denied_geolocation")}
-      {locationInstructionsMissing && (
-        <p className="mt-2 mb-0">{md("mobility:location_instructions_missing")}</p>
-      )}
-      {linkKey && (
+      {!isEmpty(buttonProperties) && (
         <>
           <p className="mt-2">
             <i className="me-1 bi bi-arrow-clockwise"></i>
             {md("mobility:reload_after_instructions")}
           </p>
-          <SumaButton href={externalLinks[linkKey](language)} as={ExternalLink}>
-            <i className={clsx("me-1", icon && `bi ${icon}`)}></i>
-            {t("mobility:activate_location_warning")}
-          </SumaButton>
+          {buttonProperties.map(({ key, icon, label }) => (
+            <SumaButton
+              key={key}
+              href={externalLinks[key](language)}
+              as={ExternalLink}
+              className="mt-2"
+            >
+              <i className={clsx("me-1", icon)}></i>
+              {label}
+            </SumaButton>
+          ))}
         </>
+      )}
+      {!isLoading && isEmpty(buttonProperties) && (
+        <p className="mt-2 mb-0">{md("mobility:location_instructions_missing")}</p>
       )}
     </Alert>
   );
@@ -62,4 +55,49 @@ const LocationInstructionsAlert = () => {
 
 export default LocationInstructionsAlert;
 
-const supportedBrowsers = ["safari", "chrome", "firefox", "edge"];
+/**
+ * Takes the user agent returns the supported browser and platform objects
+ * The objects return key, icon and label properties. The key is used to get
+ * the matching link from the externalLinks object. The icon represents a
+ * Bootstrap icon className.
+ *
+ * Some browsers and platforms will have the same links but it is okay.
+ *
+ * Only the found browser and/or platform returned.
+ *
+ * @param ua The user agent to analyze device, platform and other data
+ * @returns []
+ */
+function supportedBrowserAndPlatform(ua) {
+  // get the second word in case for "Microsoft Edge"
+  const browser = ua.device.toLowerCase().split(" ")[1] || ua.device.toLowerCase();
+  // get the first word in case for "ios (iPad)"
+  const platform = ua.platform.toLowerCase().split(" ")[0] || ua.platform.toLowerCase();
+  const result = [];
+  if (supportedBrowsers.includes(browser)) {
+    result.push({
+      key: browser,
+      icon: `bi bi-browser-${browser}`,
+      label: t("mobility:activate_device", { device: browser }),
+    });
+  }
+  if (supportedPlatforms.includes(platform)) {
+    if (platform === "macos" || ua.isIos) {
+      result.push({
+        key: "ios",
+        icon: "bi bi-apple",
+        label: t("mobility:activate_device", { device: platform }),
+      });
+    } else {
+      result.push({
+        key: platform,
+        icon: `bi bi-${platform}`,
+        label: t("mobility:activate_device", { device: platform }),
+      });
+    }
+  }
+  return result;
+}
+
+const supportedBrowsers = ["chrome", "firefox", "edge"];
+const supportedPlatforms = ["macos", "ios", "windows", "android"];
--- webapp/src/modules/externalLinks.js
+++ webapp/src/modules/externalLinks.js
@@ -3,21 +3,19 @@ const externalLinks = {
   mobilityInfoLink: "https://mysuma.org/sumaplatform#mobility",
   privacyPolicy: "https://app.mysuma.org/privacy-policy",
   sumaIntroLink: "https://mysuma.org/",
-  android: (lang) =>
-    `https://support.google.com/chrome/answer/142065?hl=${lang}&co=GENIE.Platform%3DAndroid&oco=1`,
-  chrome: (lang) =>
-    `https://support.google.com/chrome/answer/142065?hl=${lang}&co=GENIE.Platform%3DDesktop&oco=1`,
   edge: (lang) =>
     `https://support.microsoft.com/${lang}-us/windows/windows-location-service-and-privacy-3a8eee0a-5b0b-dc07-eede-2a5ca1c49088`,
   firefox: (lang) =>
     `https://support.mozilla.org/${lang}-US/kb/does-firefox-share-my-location-websites`,
-  ios: (lang) => `https://support.apple.com/${lang}-us/HT207092`,
-  safari: (lang) =>
-    `https://support.apple.com/${lang}-${lang}/guide/mac-help/allow-apps-to-detect-the-location-of-your-mac-mh35873/mac#:~:text=${
-      lang === "en"
-        ? "Specify%20which%20apps%20and%20system%20services%20can%20use%20Location%20Services"
-        : "Especificar%20qué%20apps%20y%20servicios%20del%20sistema%20pueden%20usar%20Localización"
-    }`,
+  chrome: (lang) =>
+    `https://support.google.com/chrome/answer/142065?hl=${lang}&co=GENIE.Platform%3DAndroid&oco=1`,
+  android: (lang) => `https://support.google.com/accounts/answer/3467281?hl=${lang}`,
+  windows: (lang) =>
+    `https://support.microsoft.com/${lang}-us/windows/windows-location-service-and-privacy-3a8eee0a-5b0b-dc07-eede-2a5ca1c49088`,
+  ios: (lang) =>
+    `https://support.apple.com/${lang}-${lang}/guide/personal-safety/ips9bf20ad2f/web`,
+  macos: (lang) =>
+    `https://support.apple.com/${lang}-${lang}/guide/personal-safety/ips9bf20ad2f/web`,
 };
 
 export default externalLinks;
