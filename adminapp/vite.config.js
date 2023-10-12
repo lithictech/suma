8166a478cfe9e3e8f9b6561c5494d5e8820d594c
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2023-10-12 12:18:48 -0400

    Admin initial vite package install and setup

---
+++ adminapp/vite.config.js
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint()
+  ],
+  build: {
+    outDir: "../build-webapp",
+    emptyOutDir: true,
+  },
+});
