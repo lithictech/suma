65ff1eba0fa9aa4bbfc97974ef9b61d18af1d609
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2023-10-23 18:51:13 -0400

    Setup admin product create page

--- adminapp/src/api.js
+++ adminapp/src/api.js
@@ -88,6 +88,8 @@ export default {
     get(`/adminapi/v1/commerce_offerings/${id}/picklist`, data),
 
   getCommerceProducts: (data) => get("/adminapi/v1/commerce_products", data),
+  createCommerceProduct: (data) =>
+    postForm("/adminapi/v1/commerce_products/create", data),
   getCommerceProduct: ({ id, ...data }) =>
     get(`/adminapi/v1/commerce_products/${id}`, data),
 
--- adminapp/src/pages/ProductListPage.jsx
+++ adminapp/src/pages/ProductListPage.jsx
@@ -1,5 +1,7 @@
 import api from "../api";
 import AdminLink from "../components/AdminLink";
+import FabAdd from "../components/FabAdd";
+import Link from "../components/Link";
 import ResourceTable from "../components/ResourceTable";
 import { dayjs } from "../modules/dayConfig";
 import useAsyncFetch from "../shared/react/useAsyncFetch";
@@ -28,45 +30,48 @@ export default function ProductListPage() {
     }
   );
   return (
-    <ResourceTable
-      page={page}
-      perPage={perPage}
-      search={search}
-      order={order}
-      orderBy={orderBy}
-      title="Products"
-      listResponse={listResponse}
-      listLoading={listLoading}
-      tableProps={{ sx: { minWidth: 650 }, size: "small" }}
-      onParamsChange={setListQueryParams}
-      columns={[
-        {
-          id: "id",
-          label: "ID",
-          align: "center",
-          sortable: true,
-          render: (c) => <AdminLink model={c} />,
-        },
-        {
-          id: "created_at",
-          label: "Created",
-          align: "left",
-          sortable: true,
-          render: (c) => dayjs(c.createdAt).format("lll"),
-        },
-        {
-          id: "name",
-          label: "Name",
-          align: "left",
-          render: (c) => <AdminLink model={c}>{c.name}</AdminLink>,
-        },
-        {
-          id: "vendor",
-          label: "Vendor",
-          align: "left",
-          render: (c) => c.vendor.name,
-        },
-      ]}
-    />
+    <>
+      <FabAdd component={Link} href={"/product/new"} />
+      <ResourceTable
+        page={page}
+        perPage={perPage}
+        search={search}
+        order={order}
+        orderBy={orderBy}
+        title="Products"
+        listResponse={listResponse}
+        listLoading={listLoading}
+        tableProps={{ sx: { minWidth: 650 }, size: "small" }}
+        onParamsChange={setListQueryParams}
+        columns={[
+          {
+            id: "id",
+            label: "ID",
+            align: "center",
+            sortable: true,
+            render: (c) => <AdminLink model={c} />,
+          },
+          {
+            id: "created_at",
+            label: "Created",
+            align: "left",
+            sortable: true,
+            render: (c) => dayjs(c.createdAt).format("lll"),
+          },
+          {
+            id: "name",
+            label: "Name",
+            align: "left",
+            render: (c) => <AdminLink model={c}>{c.name}</AdminLink>,
+          },
+          {
+            id: "vendor",
+            label: "Vendor",
+            align: "left",
+            render: (c) => c.vendor.name,
+          },
+        ]}
+      />
+    </>
   );
 }
