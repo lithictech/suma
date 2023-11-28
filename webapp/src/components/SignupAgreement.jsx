65f878cab7b02ce96fabcc3c4ea62cfa6302fd0e
Rob Galanakis <rob.galanakis@gmail.com>
2023-11-27 22:43:15 -0800

    Add checkbox to signup/phone form
    SignalWire is requiring this for our campaign verification.

---
+++ webapp/src/components/SignupAgreement.jsx
@@ -0,0 +1,20 @@
+import { mdp, t } from "../localization";
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+export default function SignupAgreement({ checked, onCheckedChanged, ...rest }) {
+  return (
+    <div className="d-flex">
+      <Form.Check
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onCheckedChanged(e.target.checked)}
+        aria-label="Agree to terms"
+        {...rest}
+      />
+      <div id="signup-agreement" className="ms-2 text-secondary small">
+        {mdp("auth:sign_up_agreement", { buttonLabel: t("forms:continue") })}
+      </div>
+    </div>
+  );
+}
