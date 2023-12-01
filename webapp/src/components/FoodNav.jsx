05bbe394ecd1571c2a058534099dec1f0a5aa430
Rob Galanakis <rob.galanakis@gmail.com>
2023-12-01 11:03:18 -0800

    Fix padding on cart icon

--- webapp/src/components/FoodNav.jsx
+++ webapp/src/components/FoodNav.jsx
@@ -7,29 +7,18 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
 export default function FoodNav({ offeringId, startElement, cart }) {
-  if (startElement) {
-    return (
-      <Container className={`hstack gap-3 border-0 py-2 ${guttersClass}`}>
-        {startElement && startElement}
-        <Button
-          href={`/cart/${offeringId}`}
-          variant={clsx(cart.items?.length > 0 ? "success" : "primary")}
-          className="ms-auto"
-          as={RLink}
-        >
-          <CartIcon cart={cart} className="d-flex flex-row position-relative" />
-        </Button>
-      </Container>
-    );
-  }
   return (
-    <Button
-      href={`/cart/${offeringId}`}
-      variant="success"
-      className="py-1 mt-2"
-      as={RLink}
-    >
-      <CartIcon cart={cart} />
-    </Button>
+    <Container className={`hstack gap-3 border-0 py-2 ${guttersClass}`}>
+      {startElement && startElement}
+      <Button
+        href={`/cart/${offeringId}`}
+        variant={clsx(cart.items?.length > 0 ? "success" : "primary")}
+        className="ms-auto py-1"
+        size="sm"
+        as={RLink}
+      >
+        <CartIcon cart={cart} className="d-flex flex-row position-relative" />
+      </Button>
+    </Container>
   );
 }
