0b75563984a4253bedcac5ed3c18ad93ae6ccb1a
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2024-05-06 14:00:49 -0400

    Check ledger category slug vs ledger name

--- lib/suma/api/ledgers.rb
+++ lib/suma/api/ledgers.rb
@@ -10,7 +10,9 @@ class Suma::API::Ledgers < Suma::API::V1
     desc "Return an overview of cash ledger and ledgers with transactions including balances, and recent transactions."
     get :overview do
       me = current_member
-      ledgers = (me.payment_account&.ledgers || []).select { |led| led.any_transactions? || led.slug === "cash" }
+      ledgers = (me.payment_account&.ledgers || []).select do |led|
+        led.any_transactions? || led.vendor_service_categories.first&.slug === "cash"
+      end
       lv = Suma::Payment::LedgersView.new(ledgers)
       first_page = []
       page_count = 0
