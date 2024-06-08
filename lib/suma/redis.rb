771aa70ac21467e9f489ac97829166e2e7002e34
Rob Galanakis <rob.galanakis@gmail.com>
2024-06-08 08:50:21 -0700

    Unify redis ssl verification workaround (#655)
    * Unify redis ssl verification workaround
    
    We know we have to use VERIFY_NONE on Heroku,
    this adds `Suma::Redis.conn_params` to centralize the logic.
    Use it in `Suma::RackAttack`.
    
    ---------
    
    Co-authored-by: Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>

--- lib/suma/redis.rb
+++ lib/suma/redis.rb
@@ -2,25 +2,33 @@
 
 require "appydays/configurable"
 require "redis_client"
+require "suma/postgres/model"
 
 module Suma::Redis
   include Appydays::Configurable
 
   class << self
     attr_accessor :cache
+
+    def conn_params(url, **kw)
+      params = {url:}
+      if url.start_with?("rediss:") && ENV["HEROKU_APP_ID"]
+        # rediss: schema is Redis with SSL. They use self-signed certs, so we have to turn off SSL verification.
+        # There is not a clear KB on this, you have to piece it together from Heroku and Sidekiq docs.
+        params[:ssl_params] = {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+      end
+      params.merge!(kw)
+      return params
+    end
   end
 
   configurable(:redis) do
     setting :cache_url, "redis://localhost:22007/0"
     setting :cache_url_provider, "REDIS_URL"
-    setting :verify_ssl, false
 
     after_configured do
       url = ENV.fetch(self.cache_url_provider, self.cache_url)
-      cache_params = {url:, reconnect_attempts: 1}
-      cache_params[:ssl_params] = {verify_mode: OpenSSL::SSL::VERIFY_NONE} unless
-        self.verify_ssl
-      redis_config = RedisClient.config(**cache_params)
+      redis_config = RedisClient.config(**self.conn_params(url, reconnect_attempts: 1))
       self.cache = redis_config.new_pool(
         timeout: Suma::Postgres::Model.pool_timeout,
         size: Suma::Postgres::Model.max_connections,
---
+++ spec/suma/redis_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "suma/redis"
+
+RSpec.describe Suma::Redis do
+  describe "#conn_params" do
+    it "returns keyword arguments" do
+      params = described_class.conn_params("redis://localhost:1234/0", reconnect_attempts: 1, timeout: 1.0)
+      expect(params).to include(url: "redis://localhost:1234/0", reconnect_attempts: 1, timeout: 1.0)
+    end
+
+    it "returns ssl_params when using heroku redis" do
+      ssl_schema_url = "rediss://"
+      none_ssl_schema_url = "redis://"
+
+      expect(described_class.conn_params(none_ssl_schema_url)).to_not include(:ssl_params)
+      expect(described_class.conn_params(ssl_schema_url)).to_not include(:ssl_params)
+      ENV["HEROKU_APP_ID"] = "a1b2bc"
+      expect(described_class.conn_params(ssl_schema_url)).to include(ssl_params: {verify_mode: 0})
+      expect(described_class.conn_params(none_ssl_schema_url)).to_not include(:ssl_params)
+    ensure
+      ENV.delete("HEROKU_APP_ID")
+    end
+  end
+end
