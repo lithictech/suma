db3cc8326dd8b46d1cfa25788df2a7d8657509b6
Denilson Velasquez <66847768+DeeTheDev@users.noreply.github.com>
2024-09-16 13:37:25 -0400

    Refactor survey data structure
    - Use object key values rather than array of objects data structure
    - Refactor functions

--- webapp/src/components/Survey.jsx
+++ webapp/src/components/Survey.jsx
@@ -1,5 +1,5 @@
 import { t } from "../localization";
-import merge from "lodash/merge";
+import isEmpty from "lodash/isEmpty";
 import React from "react";
 import { FormLabel } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -7,41 +7,13 @@ import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
 
 export default function Survey({ feature, onSubmit }) {
-  let survey = [];
-  const handleCheck = (questionKey, event) => {
-    const newAnswerKey = event.target.id;
-    if (event.target.checked) {
-      const questionObj = survey.find((item) => item.questionKey === questionKey);
-      if (!questionObj) {
-        // Add first time answers
-        survey.push({ questionKey: questionKey, answerKeys: [newAnswerKey] });
-        return;
-      }
-      const newObj = questionObj.answerKeys.push(newAnswerKey);
-      survey.map((obj) => (obj === questionObj ? merge(questionObj, newObj) : obj));
+  let survey = {};
+  const handleChange = (questionKey, newVal) => {
+    if (isEmpty(newVal.answerKeys)) {
+      delete survey[questionKey];
       return;
     }
-    // Delete answer from question
-    survey = survey.map((obj) => {
-      if (obj.questionKey === questionKey) {
-        obj.answerKeys = obj.answerKeys.filter((k) => k !== newAnswerKey);
-      }
-      return obj;
-    });
-  };
-
-  const handleRadio = (questionKey, event) => {
-    const questionObj = survey.find((item) => item.questionKey === questionKey);
-    if (!questionObj) {
-      survey.push({ questionKey: questionKey, answerKeys: [event.target.id] });
-      return;
-    }
-    survey = survey.map((obj) => {
-      if (obj.questionKey === questionKey) {
-        obj.answerKeys = [event.target.id];
-      }
-      return obj;
-    });
+    survey[questionKey] = newVal;
   };
   return (
     <Form noValidate onSubmit={(e) => onSubmit(e, survey)}>
@@ -50,39 +22,58 @@ export default function Survey({ feature, onSubmit }) {
           <Options
             questionKey="shop_for_food"
             optionKeys={shopForFoodOptionKeys}
-            handleChange={handleCheck}
+            survey={survey}
+            handleChange={(newValue) => handleChange("shop_for_food", newValue)}
           />
         )}
         {feature === "utilities" && (
           <Options
             questionKey="household_utilities"
             optionKeys={householdUtilitiesOptionKeys}
-            handleChange={handleCheck}
+            survey={survey}
+            handleChange={(newValue) => handleChange("household_utilities", newValue)}
           />
         )}
         <Options
           questionKey="member_description"
           optionKeys={memberDescriptionOptionKeys}
-          handleChange={handleRadio}
+          survey={survey}
+          handleChange={(newValue) => handleChange("member_description", newValue)}
           radio
         />
         <Options
           questionKey="want_to_learn"
           optionKeys={wantToLearnOptionKeys}
+          survey={survey}
+          handleChange={(newValue) => handleChange("want_to_learn", newValue)}
           feature={feature}
-          handleChange={handleCheck}
         />
       </Stack>
       <div className="button-stack mt-4">
         <Button type="submit" variant="outline-primary">
           {t("common:join_waitlist")}
-        </Button>{" "}
+        </Button>
       </div>
     </Form>
   );
 }
 
-function Options({ questionKey, optionKeys, feature, radio, handleChange }) {
+function Options({ questionKey, optionKeys, survey, feature, radio, handleChange }) {
+  function handleCheckboxChange(event) {
+    if (event.target.checked) {
+      handleChange({
+        answerKeys: [event.target.id, ...(survey[questionKey]?.answerKeys || [])],
+      });
+      return;
+    }
+    const filteredKeys = survey[questionKey].answerKeys.filter(
+      (k) => k !== event.target.id
+    );
+    handleChange({ answerKeys: filteredKeys });
+  }
+  function handleRadioChange(event) {
+    handleChange({ answerKeys: [event.target.id] });
+  }
   return (
     <div>
       <FormLabel>{t(`survey:${questionKey}:label`)}</FormLabel>
@@ -98,7 +89,9 @@ function Options({ questionKey, optionKeys, feature, radio, handleChange }) {
             feature: t(feature + ":title").toLowerCase(),
           })}
           type={radio ? "radio" : "checkbox"}
-          onChange={(event) => handleChange(questionKey, event)}
+          onChange={(event) =>
+            radio ? handleRadioChange(event) : handleCheckboxChange(event)
+          }
         />
       ))}
     </div>
