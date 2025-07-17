ec0068e70f132c74092f53303cf6e6476e009553
Rob Galanakis <rob.galanakis@gmail.com>
2025-07-17 09:14:03 -0700

    Add sidekiq deploy marking to release
    Add tests for sidekiq and release tasks,
    and add rake tasks to retry sidekiq dead and retry sets.
    
    Make sure rake tasks are re-enabled after running a test.

--- spec/tasks/bootstrap_spec.rb
+++ spec/tasks/bootstrap_spec.rb
@@ -3,16 +3,31 @@
 require "suma/tasks/bootstrap"
 
 RSpec.describe Suma::Tasks::Bootstrap, :db do
+  before(:all) do
+    described_class.new
+  end
+
   it "runs successfully" do
+    stub_const("Suma::RACK_ENV", "development")
     req_ip = stub_request(:get, "http://whatismyip.akamai.com/").
       to_return(status: 200, body: "1.1.1.1")
     req_loc = stub_request(:get, "http://ip-api.com/json/1.1.1.1").
       to_return(status: 200, body: {lat: "45", lon: "-120"}.to_json, headers: {"Content-Type" => "application/json"})
-    expect { described_class.new.run_task }.to_not raise_error
+    invoke_rake_task("bootstrap")
     expect(Suma::Member.all).to have_length(1)
     expect(Suma::Mobility::Vehicle.all).to have_length(2)
     expect(Suma::Commerce::Offering.all).to have_length(2)
     expect(req_ip).to have_been_made.times(2)
     expect(req_loc).to have_been_made.times(2)
+  end
+
+  it "errors if not in development" do
+    expect { invoke_rake_task("bootstrap") }.to raise_error(/only run this in development/)
+  end
+
+  it "errors if the database is not empty" do
+    stub_const("Suma::RACK_ENV", "development")
+    Suma::Fixtures.member.create
+    expect { invoke_rake_task("bootstrap") }.to raise_error(/only run with a fresh database/)
   end
 end
---
+++ spec/tasks/sidekiq_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "suma/tasks/sidekiq"
+
+RSpec.describe Suma::Tasks::Sidekiq, sidekiq: :disable do
+  before(:all) do
+    described_class.new
+  end
+
+  describe "reset" do
+    it "clears the redis DB" do
+      Sidekiq.redis { |c| c.set("testkey", "1") }
+      expect(Sidekiq.redis { |c| c.get("testkey") }).to eq("1")
+      invoke_rake_task("sidekiq:reset")
+      expect(Sidekiq.redis { |c| c.get("testkey") }).to be_nil
+    end
+  end
+
+  describe "retry_all" do
+    it "enqueues all retry set jobs for retry" do
+      # Tested for coverage, sorry.
+      invoke_rake_task("sidekiq:retry_all")
+      expect(Sidekiq::RetrySet.new.size).to eq(0)
+    end
+  end
+
+  describe "retry_all_dead" do
+    it "enqueues all dead set jobs for retry" do
+      # Tested for coverage, sorry.
+      invoke_rake_task("sidekiq:retry_all_dead")
+      expect(Sidekiq::DeadSet.new.size).to eq(0)
+    end
+  end
+end
