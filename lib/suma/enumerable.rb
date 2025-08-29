9749b031f51b842da9eb263ab394214a3f705274
Rob Galanakis <rob.galanakis@gmail.com>
2025-08-29 16:47:42 -0700

    Add Suma::Enumerable#one!

--- lib/suma/enumerable.rb
+++ lib/suma/enumerable.rb
@@ -15,4 +15,11 @@ module Suma::Enumerable
   module_function def group_and_count(enumerable)
     return group_and_count_by(enumerable) { |k| k }
   end
+
+  # Return the only item in the enumerable if it has a length one.
+  # Raise an ArgumentError otherwise.
+  module_function def one!(enumerable)
+    return enumerable.first if enumerable.length == 1
+    raise ArgumentError, "must have exactly 1 item, got #{enumerable.count}"
+  end
 end
