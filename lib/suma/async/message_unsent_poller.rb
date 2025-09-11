199f13dbf65fa5e50a93ba955694bbffd3723e89
Rob Galanakis <rob.galanakis@gmail.com>
2025-09-11 10:21:45 -0700

    Rename 'emailer' job
    This job sends unsent messages.
    This was an old name from old projects;
    name it more in line with what it actually does.

--- lib/suma/async/message_unsent_poller.rb
+++ lib/suma/async/message_unsent_poller.rb
@@ -2,18 +2,16 @@
 
 require "amigo/scheduled_job"
 
-class Suma::Async::Emailer
+class Suma::Async::MessageUnsentPoller
   extend Amigo::ScheduledJob
   include Suma::Async::JobUtils
 
   sidekiq_options(Suma::Async.cron_job_options)
-  cron "* * * * *"
+  cron "*/5 * * * *"
   splay 5.seconds
 
   def _perform
     sent = Suma::Message.send_unsent
     self.set_job_tags(sent_messages: sent.count)
   end
-
-  Amigo.register_job(self)
 end
