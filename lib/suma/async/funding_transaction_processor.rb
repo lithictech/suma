58ca28af8fe39653c4d49d4dfbba9241985141c5
Rob Galanakis <rob.galanakis@gmail.com>
2025-11-03 08:19:24 -0800

    Add job tags to transaction processors

--- lib/suma/async/funding_transaction_processor.rb
+++ lib/suma/async/funding_transaction_processor.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 require "amigo/scheduled_job"
+require "suma/async/job_utils"
 
 class Suma::Async::FundingTransactionProcessor
   extend Amigo::ScheduledJob
+  include Suma::Async::JobUtils
 
   sidekiq_options(Suma::Async.cron_job_options)
   cron "*/5 * * * *"
   splay 30.seconds
 
   def _perform
-    Suma::Payment::FundingTransaction.where(status: "created").each { |x| x.process(:collect_funds) }
-    Suma::Payment::FundingTransaction.where(status: "collecting").each { |x| x.process(:collect_funds) }
+    Suma::Payment::FundingTransaction.where(status: "created").each { |x| self.process(x, :collect_funds) }
+    Suma::Payment::FundingTransaction.where(status: "collecting").each { |x| self.process(x, :collect_funds) }
+  end
+
+  def process(tx, m)
+    member = tx.originating_payment_account.member
+    tags = {member_id: member&.id, member_name: member&.name, funding_transaction_id: tx.id}
+    self.with_log_tags(tags) { tx.process(m) }
   end
 end
--- lib/suma/async/payout_transaction_processor.rb
+++ lib/suma/async/payout_transaction_processor.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 require "amigo/scheduled_job"
+require "suma/async/job_utils"
 
 class Suma::Async::PayoutTransactionProcessor
   extend Amigo::ScheduledJob
+  include Suma::Async::JobUtils
 
   sidekiq_options(Suma::Async.cron_job_options)
   cron "*/5 * * * *"
   splay 30.seconds
 
   def _perform
-    Suma::Payment::PayoutTransaction.where(status: "created").each { |x| x.process(:send_funds) }
-    Suma::Payment::PayoutTransaction.where(status: "sending").each { |x| x.process(:send_funds) }
+    Suma::Payment::PayoutTransaction.where(status: "created").each { |x| self.process(x, :send_funds) }
+    Suma::Payment::PayoutTransaction.where(status: "sending").each { |x| self.process(x, :send_funds) }
+  end
+
+  def process(tx, m)
+    member = tx.originating_payment_account.member
+    tags = {member_id: member&.id, member_name: member&.name, payout_transaction_id: tx.id}
+    self.with_log_tags(tags) { tx.process(m) }
   end
 end
