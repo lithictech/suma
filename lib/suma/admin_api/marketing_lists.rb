83a782d6a241ca3fee03790d4fd68f4389b12238
Rob Galanakis <rob.galanakis@gmail.com>
2025-12-04 16:25:55 -0800

    Rebuild and destroy marketing lists

--- lib/suma/admin_api/marketing_lists.rb
+++ lib/suma/admin_api/marketing_lists.rb
@@ -40,7 +40,7 @@ class Suma::AdminAPI::MarketingLists < Suma::AdminAPI::V1
       Suma::Marketing::List,
       DetailedListEntity,
       around: lambda do |rt, m, &block|
-        rt.adminerror!(403, "Managed lists cannot be edited") if m.managed?
+        rt.adminerror!(403, "Managed lists cannot be edited", code: "marketing_list_managed") if m.managed?
         members = rt.params.delete(:members)
         block.call
         m.member_pks = members.map { |l| l.fetch(:id) } if
@@ -50,6 +50,36 @@ class Suma::AdminAPI::MarketingLists < Suma::AdminAPI::V1
       params do
         optional :label, type: String
         optional :members, type: Array
+      end
+    end
+
+    Suma::AdminAPI::CommonEndpoints.destroy(
+      self,
+      Suma::Marketing::List,
+      DetailedListEntity,
+    )
+
+    route_param :id, type: Integer do
+      helpers do
+        def lookup
+          Suma::Marketing::List[params[:id]]
+        end or forbidden!
+      end
+
+      post :rebuild do
+        check_admin_role_access!(:read, :marketing_sms)
+        list = lookup
+        sleep(1)
+        adminerror!(403, "Only managed lists can be rebuilt", code: "marketing_list_unmanaged") unless list.managed
+        spec = Suma::Marketing::List::Specification.gather_all.find { |spec| spec.full_label == list.label }
+        if spec.nil?
+          msg = "Could not find a list specification- this list should be unmanaged, please alert a developer"
+          adminerror!(403, msg, code: "marketing_list_spec_missing")
+        end
+        Suma::Marketing::List.rebuild(spec)
+        list.refresh
+        status 200
+        present list, with: DetailedListEntity
       end
     end
   end
