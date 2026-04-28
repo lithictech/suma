5b4719af70ccfcf864b6cceb8e754262f5d0986c
Rob Galanakis <rob.galanakis@gmail.com>
2026-04-28 09:45:12 -0700

    AdminLink: Make label explit
    Using .label instead of .id by default ends up being
    bad behavior since we have a redundant label shown
    in many lists. Instead, pass in `label` to enable
    showing the label.

--- adminapp/src/components/AdminLink.jsx
+++ adminapp/src/components/AdminLink.jsx
@@ -1,14 +1,19 @@
 import relativeLink from "../modules/relativeLink";
 import MatLink from "@mui/material/Link";
+import get from "lodash/get";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 const AdminLink = React.forwardRef(function AdminLink(
-  { href, to, model, children, ...rest },
+  { href, to, model, children, label, ...rest },
   ref
 ) {
   const [newTo, isRelative] = relativeLink(model?.adminLink || href || to || "");
-  const newProps = { ...rest, ref, children: children || model?.label || model?.id };
+  const newProps = {
+    ...rest,
+    ref,
+    children: children || getLabel(model, label) || model?.id,
+  };
   if (isRelative) {
     newProps.component = RouterLink;
     newProps.to = newTo;
@@ -19,6 +24,16 @@ const AdminLink = React.forwardRef(function AdminLink(
 });
 
 export default AdminLink;
+
+function getLabel(model, label) {
+  if (!label || !model) {
+    return null;
+  }
+  if (label === true) {
+    return model.label;
+  }
+  return get(model, label).label;
+}
 
 AdminLink.Array = function (array, cb, sep = <></>) {
   return array.map((o, i) => (
