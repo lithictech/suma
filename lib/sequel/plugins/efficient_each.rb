4a3a4a018c38c98910068fd04d7bf449895e4541
Rob Galanakis <rob.galanakis@gmail.com>
2026-05-24 14:00:25 -0700

    Add association loading performance plugins
    Add two plugins to help with association loading performance.
    
    - large association warning: warn when associations over a certain size are loaded. Prevents unexpectedly blowing out memory.
    - efficient each: Allows 1) reusing a loaded association array, 2) streaming a not-loaded association from the database, and 3) storing a streamed association into the array if it's just one page. Move each_cursor_page to this plugin.

---
+++ lib/sequel/plugins/efficient_each.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+module Sequel::Plugins::EfficientEach
+  class UnknownAssociation < ArgumentError; end
+
+  DEFAULT_OPTIONS = {
+    page_size: 100,
+  }.freeze
+
+  class << self
+    def configure(model, opts=DEFAULT_OPTIONS)
+      opts = DEFAULT_OPTIONS.merge(opts)
+      model.efficient_each_page_size = opts[:page_size]
+    end
+  end
+
+  module ClassMethods
+    attr_accessor :efficient_each_page_size
+
+    def inherited(subclass)
+      super
+      [:efficient_each_page_size].each do |m|
+        subclass.send("#{m}=", self.send(m))
+      end
+    end
+  end
+
+  module DatasetMethods
+    # Call a block for each row in a dataset.
+    # This is the same as paged_each or use_cursor.each, except that for each page,
+    # rows are re-fetched using self.where(primary_key => [pks]).all to enable eager loading.
+    #
+    # @param page_size [Integer] Size of each page. Smaller uses less memory.
+    # @param order [Symbol] Column to order by. Default to primary key.
+    # @param yield_page [true,false] If true, yield the page to the block, rather than individual rows.
+    #   Helpful when bulk processing.
+    #
+    # (Note that paged_each does not do eager loading, which makes enumerating model associations very slow)
+    def each_cursor_page(page_size: nil, order: nil, yield_page: false, &block)
+      raise LocalJumpError unless block
+      raise "dataset requires a use_cursor method, class may need `extension(:pagination)`" unless
+        self.respond_to?(:use_cursor)
+      model = self.model
+      page_size ||= model.efficient_each_page_size
+      pk = model.primary_key
+      order ||= pk
+      current_chunk_pks = []
+      order = [order] unless order.respond_to?(:to_ary)
+      self.naked.select(pk).order(*order).use_cursor(rows_per_fetch: page_size, hold: true).each do |row|
+        current_chunk_pks << row[pk]
+        next if current_chunk_pks.length < page_size
+        page = model.where(pk => current_chunk_pks).order(*order).all
+        current_chunk_pks.clear
+        yield_page ? yield(page) : page.each(&block)
+      end
+      remainder = model.where(pk => current_chunk_pks).order(*order).all
+      yield_page && !remainder.empty? ? yield(remainder) : remainder.each(&block)
+    end
+  end
+
+  module InstanceMethods
+    def efficient_each(association_name, &)
+      return enum_for(:efficient_each, association_name) unless block_given?
+
+      assoc = self.class.association_reflection(association_name)
+      raise UnknownAssociation, "#{self.class.name} has no association :#{association_name}" if
+        assoc.nil?
+      loaded = self.associations[association_name]
+      unless loaded.nil?
+        loaded.each(&)
+        return nil
+      end
+      dataset = self.send(assoc.fetch(:dataset_method))
+      pagecount = 0
+      prev_page = []
+      dataset.each_cursor_page(yield_page: true) do |page|
+        pagecount += 1
+        prev_page = page
+        page.each(&)
+      end
+      self.associations[association_name] = prev_page if pagecount < 2
+      return nil
+    end
+  end
+end
---
+++ spec/sequel/plugins/large_association_warning_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "sequel/plugins/large_association_warning"
+
+RSpec.describe Sequel::Plugins::LargeAssociationWarning, :db do
+  before(:all) do
+    @db = Sequel.connect(ENV.fetch("DATABASE_URL"))
+    @db.drop_table?(:largeassocwarn)
+    @db.create_table(:largeassocwarn) do
+      primary_key :id
+      foreign_key :parent_id, :largeassocwarn
+    end
+  end
+  after(:all) do
+    @db.disconnect
+  end
+
+  it "can use its default callback" do
+    cls = Class.new(Sequel::Model(:largeassocwarn)) do
+      plugin :large_association_warning, threshold: 2
+      many_to_one :parent, class: self
+      one_to_many :children, class: self, key: :parent_id
+    end
+
+    parent = cls.create
+    Array.new(3) { cls.create(parent:) }
+    parent.refresh.children
+  end
+
+  it "warns about large associations" do
+    calls = []
+    cls = Class.new(Sequel::Model(:largeassocwarn)) do
+      plugin :large_association_warning, threshold: 5, callback: ->(*args) { calls << args }
+      many_to_one :parent, class: self
+      one_to_many :children, class: self, key: :parent_id
+    end
+
+    parent = cls.create
+    Array.new(5) { cls.create(parent:) }
+    parent.refresh.children
+    expect(calls).to be_empty
+    cls.create(parent:)
+    expect(calls).to be_empty
+    parent.refresh.children
+    expect(calls).to contain_exactly([parent, :children, have_length(6)])
+    # Do not re-warn
+    parent.refresh.children
+    expect(calls).to contain_exactly([parent, :children, have_length(6)])
+  end
+
+  it "copies configuration to subclasses" do
+    base = Class.new(Sequel::Model(:largeassocwarn)) do
+      plugin :large_association_warning, threshold: 5
+    end
+    sub = Class.new(base)
+
+    expect(base.large_association_warning_threshold).to eq(5)
+    expect(sub.large_association_warning_threshold).to eq(5)
+  end
+end
