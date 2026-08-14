f4dbe30e4e99e83bb10250e1777ef508bff31156
Rob Galanakis <rob.galanakis@gmail.com>
2026-08-14 10:06:59 -0700

    fix a missing entity declaration

--- lib/suma/api/commerce.rb
+++ lib/suma/api/commerce.rb
@@ -343,7 +343,7 @@ class Suma::API::Commerce < Suma::API::V1
     expose :offering, with: OfferingEntity do |instance|
       instance
     end
-    expose_array :items do |_, opts|
+    expose_array :items, PricedOfferingProductEntity do |_, opts|
       opts.fetch(:items)
     end
     expose_array :vendors, VendorEntity do |_, opts|
